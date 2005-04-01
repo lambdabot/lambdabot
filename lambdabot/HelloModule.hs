d5b4bee7cf4adfd93271e7a47466ab1be151f649
jlouis <jlouis@mongers.org>
2005-04-01 22:23:19 +0000

    trailing whitespace

--- lambdabot/HelloModule.hs
+++ lambdabot/HelloModule.hs
@@ -1,5 +1,5 @@
 module HelloModule where
--- 	$Id: HelloModule.hs,v 1.5 2003/07/29 13:03:02 eris Exp $	
+-- 	$Id: HelloModule.hs,v 1.5 2003/07/29 13:03:02 eris Exp $
 import IRC
 
 newtype HelloModule = HelloModule ()
