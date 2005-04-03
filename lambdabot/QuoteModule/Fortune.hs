1ea2eaee6f285f552c072c1ccf40a0215db74a6b
jlouis <jlouis@mongers.org>
2005-04-03 14:02:54 +0000

    Haddock stringify Fortune Quote Module

--- lambdabot/QuoteModule/Fortune.hs
+++ lambdabot/QuoteModule/Fortune.hs
@@ -20,39 +20,55 @@ import Posix
 
 -- 	$Id: Fortune.hs,v 1.3 2003/07/29 13:41:49 eleganesh Exp $
 
+-- | The 'path' component is a string to the location where the fortune files
+--   are located. On some systems, this is /usr/share/games/fortunes, on others
+--   this is /usr/share/games/fortune. Alter this to suit your configuration
+--
+--   TODO: Move this to a generic Config file
 path :: String
 path = "/usr/share/games/fortunes/"
 
+-- | The 'filelist' function returns a List of fortune files from the
+--   configured 'path' directory.
 filelist :: IO [String]
 filelist = do filelist'<- C.catch (getDirectoryContents path)
                                   (\_ -> return [])
               let files = filter (not . isSuffixOf ".dat") filelist'
               join (return (filterM isFile (map (path ++) files)))
 
+-- | Select a random fortune file by using the random number generator
+--   given.
 fileRandom :: (RandomGen a) => a -> IO (String, a)
 fileRandom rng = liftM2 getRandItem filelist (return rng)
 
+-- | Parse a file of fortunes into a list of the fortunes in the file.
 fortunesParse :: FilePath -> IO [String]
 fortunesParse filename = do
     rawfs <- C.catch (readFile filename)
                      (\_ -> return "Couldn't find fortune file")
     return (map unlines $ splines $ lines rawfs)
 
+-- | Given a FilePath of a fortune file, select a random fortune from it
+--   and return it along with the altered new seed of the RNG.
 fortuneRandom :: (RandomGen g) => FilePath -> g -> IO (String, g)
 fortuneRandom filename rng
     = do
       fortunesList <- fortunesParse filename
       return (getRandItem fortunesList rng)
 
+-- | Given a RNG, return a random fortune from the fortune files
 randFortune :: (RandomGen a) => a -> IO (String, a)
 randFortune rng = join (liftM (uncurry fortuneRandom) $ fileRandom rng)
 
+-- | Split a fortune file into a list of fortures by breaking it up on
+--   the '%' characters
 splines :: [[Char]] -> [[[Char]]]
 splines [[]] = []
 splines s    =  let (l,s') = break ("%"==) s
                 in l : case s' of []      -> []
                                   (_:s'') -> splines s''
 
+-- | 'isFile' is a predicate wheter or not a given FilePath is a file.
 isFile :: FilePath -> IO Bool
 isFile x = do
            fs <- getFileStatus x
