433b37635b6216b42f9f0b7ee6a74359f05745c3
Don Stewart <dons@cse.unsw.edu.au>
2005-04-05 05:00:57 +0000

    Clean up GenStaticModules a bit.

--- lambdabot/GenStaticModules.hs
+++ lambdabot/GenStaticModules.hs
@@ -1,30 +1,45 @@
+--
+-- Preprocessor for setting which modules are statically linked
+--
 module Main where
 
 import Char
 
+main :: IO ()
 main = do modules <- readFile "StaticModules.conf"
           writeFile "StaticModules.hs" $ unlines $ process $ lines modules
 
+process :: [[Char]] -> [[Char]]
 process modules = process' (filter isValid modules)
+    where
+        isValid :: [Char] -> Bool
+        isValid [] = False
+        isValid ('#':_) = False
+        isValid _ = True
 
-isValid [] = False
-isValid ('#':_) = False
-isValid _ = True
+        process' :: [[Char]] -> [[Char]]
+        process' modules = concat [begin, map doimport modules, 
+                                   middle, map doload modules]
 
-process' modules = begin ++ map doimport modules 
-                ++ middle ++ map doload modules
-
+begin :: [[Char]]
 begin = ["module StaticModules (loadStaticModules) where",
          "import IRC",
          ""]
 
-doimport name = "import " ++ upperise name ++ "Module"
+doimport :: [Char] -> [Char]
+doimport name = "import " ++ (clean . upperise) name ++ "Module"
 
+upperise :: [Char] -> [Char]
 upperise (c:cs) = toUpper c:cs
 
+clean :: [Char] -> [Char]
+clean = reverse . dropWhile (== ' ') . reverse
+
+middle :: [[Char]]
 middle = ["",
           "loadStaticModules :: LB ()",
           "loadStaticModules",
           " = do"]
 
-doload name = "        ircInstallModule " ++ name ++ "Module"
+doload :: [Char] -> [Char]
+doload name = " ircInstallModule " ++ name ++ "Module"
