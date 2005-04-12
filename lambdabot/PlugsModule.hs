fad32ccca69996a108bced6be1ca0c44da0e0a49
Don Stewart <dons@cse.unsw.edu.au>
2005-04-12 06:03:24 +0000

    Clean up some more @plugs error messages

--- lambdabot/PlugsModule.hs
+++ lambdabot/PlugsModule.hs
@@ -52,21 +52,25 @@ plugs src = do
 -- Clean up runplugs' output
 --
 clean :: String -> String
-clean s | Just _         <- no_io      `matchRegex`   s = "No IO allowed\n"
-        | Just _         <- terminated `matchRegex`   s = "Terminated\n"
-        | Just _         <- stack_o_f  `matchRegex`   s = "Stack overflow\n"
-        | Just (a,_,b,_) <- filename `matchRegexAll`  s = a ++ clean b
-        | Just (a,_,b,_) <- filepath `matchRegexAll`  s = a ++ clean b
-        | Just (_,m,_,_) <- hsplugins `matchRegexAll` s = m
+clean s | Just _         <- no_io      `matchRegex`    s = "No IO allowed\n"
+        | Just _         <- terminated `matchRegex`    s = "Terminated\n"
+        | Just _         <- stack_o_f  `matchRegex`    s = "Stack overflow\n"
+        | Just (_,m,_,_) <- notinscope `matchRegexAll` s = m
+        | Just (_,m,_,_) <- ambiguous  `matchRegexAll` s = m
+        | Just (_,m,_,_) <- hsplugins `matchRegexAll`  s = m
+        | Just (a,_,b,_) <- filename `matchRegexAll`   s = a ++ clean b
+        | Just (a,_,b,_) <- filepath `matchRegexAll`   s = a ++ clean b
         | otherwise      = s
     where
         -- s/<[^>]*>:[^:]: //
         filepath   = mkRegex "\n?/[^\\.]*.hs:[^:]*:\n* *"
         filename   = mkRegex "\n?<[^>]*>:[^:]*:\n* *"
         terminated = mkRegex "waitForProc"
+        notinscope = mkRegex "Variable not in scope:[^\n]*"
         stack_o_f  = mkRegex "Stack space overflow"
         hsplugins  = mkRegex "Compiled, but didn't create object"
         no_io      = mkRegex "No instance for \\(Show \\(IO"
+        ambiguous  = mkRegex "Ambiguous type variable `a\' in the top-level constraint"
 
 ------------------------------------------------------------------------
 --
