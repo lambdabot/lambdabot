380b7e13d8e570fe8df28cea321c0921d34aa43f
jlouis <jlouis@mongers.org>
2005-04-12 14:11:18 +0000

    Document Config.hs

--- lambdabot/Config.hs
+++ lambdabot/Config.hs
@@ -1,16 +1,21 @@
 
 module Config where
 
+-- | The 'Config' type provides configurations for lambdabot. It is used
+--   when lambdabot is started to determine the name of lambdabot, what
+--   IRC-network lambdabot should join, which channels lambdabot should
+--   join upon successful connection, etc.
 data Config = Config {
-        name      :: String,
-        userinfo  :: String,
-        host      :: String,
-        port      :: Int,
-        verbose   :: Bool,
-        moresize  :: Int,
-        autojoin  :: [String],
-        admins    :: [String],
-        proxy     :: Maybe ([Char], Integer)
+        name      :: String, -- ^ The nickname of lambdabot
+        userinfo  :: String, -- ^ The userinfo string for lambdabot
+        host      :: String, -- ^ Host to join
+        port      :: Int,    -- ^ The port number to use on the host
+        verbose   :: Bool,   -- ^ Should lambdabot be verbose?
+        moresize  :: Int,    -- ^ How many lines is output before @more?
+        autojoin  :: [String], -- ^ List of channels to autojoin
+        admins    :: [String], -- ^ List of nicknames that are admins
+        proxy     :: Maybe ([Char], Integer) -- ^ An eventual proxy given as
+	                                     --   a pair of host and port.
 }
 
 --
