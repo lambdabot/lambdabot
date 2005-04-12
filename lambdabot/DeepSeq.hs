a616fb5f3c439cf9b9d5030a125f366b89d9712d
jlouis <jlouis@mongers.org>
2005-04-12 17:59:31 +0000

    Formatting

--- lambdabot/DeepSeq.hs
+++ lambdabot/DeepSeq.hs
@@ -1,9 +1,12 @@
 module DeepSeq where
 
--- Cut and paste from 
+-- The 'DeepSeq' module provides 2 strict deep sequencing operators,
+-- ``deepSeq'' and ``$!!''. 
+--
+-- Cut and paste from
 -- http://www.haskell.org/pipermail/haskell/2001-August/007712.html
 
-class  DeepSeq a  where
+class DeepSeq a where
   deepSeq :: a -> b -> b
   deepSeq = seq			-- default, for simple cases
 
@@ -19,18 +22,46 @@ instance  (DeepSeq a) => DeepSeq [a]  where
   deepSeq [] y = y
   deepSeq (x:xs) y = deepSeq x $ deepSeq xs y
 
-instance  (DeepSeq a,DeepSeq b) => DeepSeq (a,b)  where
-  deepSeq (a,b) y = deepSeq a $ deepSeq b y
-instance  (DeepSeq a,DeepSeq b,DeepSeq c) => DeepSeq (a,b,c)  where
-  deepSeq (a,b,c) y = deepSeq a $ deepSeq b $ deepSeq c y
-instance  (DeepSeq a,DeepSeq b,DeepSeq c,DeepSeq d) => DeepSeq (a,b,c,d) where
-  deepSeq (a,b,c,d) y = deepSeq a $ deepSeq b $ deepSeq c $ deepSeq d y
-instance  (DeepSeq a,DeepSeq b,DeepSeq c,DeepSeq d,DeepSeq e) => DeepSeq (a,b,c,d,e)  where
-  deepSeq (a,b,c,d,e) y = deepSeq a $ deepSeq b $ deepSeq c $ deepSeq d $ deepSeq e y
-instance  (DeepSeq a,DeepSeq b,DeepSeq c,DeepSeq d,DeepSeq e,DeepSeq f) => DeepSeq (a,b,c,d,e,f)  where
-  deepSeq (a,b,c,d,e,f) y = deepSeq a $ deepSeq b $ deepSeq c $ deepSeq d $ deepSeq e $ deepSeq f y
-instance  (DeepSeq a,DeepSeq b,DeepSeq c,DeepSeq d,DeepSeq e,DeepSeq f,DeepSeq g) => DeepSeq (a,b,c,d,e,f,g)  where
-  deepSeq (a,b,c,d,e,f,g) y = deepSeq a $ deepSeq b $ deepSeq c $ deepSeq d $ deepSeq e $ deepSeq f $ deepSeq g y
+instance  (DeepSeq a, DeepSeq b) => DeepSeq (a, b)  where
+  deepSeq (a, b) y = deepSeq a $ deepSeq b y
+
+instance  (DeepSeq a, DeepSeq b, DeepSeq c) => DeepSeq (a, b, c)  where
+  deepSeq (a, b, c) y = deepSeq a $ deepSeq b $ deepSeq c y
+
+instance  (DeepSeq a, DeepSeq b, DeepSeq c, DeepSeq d)
+    => DeepSeq (a, b, c, d) where
+  deepSeq (a, b, c, d) y = deepSeq a
+			   $ deepSeq b
+			   $ deepSeq c
+			   $ deepSeq d y
+
+instance  (DeepSeq a, DeepSeq b, DeepSeq c, DeepSeq d, DeepSeq e)
+    => DeepSeq (a, b, c, d, e)  where
+  deepSeq (a, b, c, d, e) y = deepSeq a
+			      $ deepSeq b
+			      $ deepSeq c
+			      $ deepSeq d
+			      $ deepSeq e y
+
+instance  (DeepSeq a, DeepSeq b, DeepSeq c, DeepSeq d, DeepSeq e, DeepSeq f)
+    => DeepSeq (a, b, c, d, e, f)  where
+  deepSeq (a, b, c, d, e, f) y = deepSeq a
+				 $ deepSeq b
+				 $ deepSeq c
+				 $ deepSeq d
+				 $ deepSeq e
+				 $ deepSeq f y
+
+instance  (DeepSeq a, DeepSeq b, DeepSeq c, DeepSeq d,
+	   DeepSeq e, DeepSeq f, DeepSeq g)
+    => DeepSeq (a, b, c, d, e, f, g)  where
+  deepSeq (a, b, c, d, e, f, g) y = deepSeq a
+				    $ deepSeq b
+				    $ deepSeq c
+				    $ deepSeq d
+				    $ deepSeq e
+				    $ deepSeq f
+				    $ deepSeq g y
 
 instance  DeepSeq Bool  where
 instance  DeepSeq Char  where
