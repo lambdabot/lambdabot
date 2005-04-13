61217ef9803ab40b050ccecc7101aaa8a9badf9a
jlouis <jlouis@mongers.org>
2005-04-13 16:00:19 +0000

    Initial Workings of FactModule

--- lambdabot/FactModule.hs
+++ lambdabot/FactModule.hs
@@ -1,100 +1,56 @@
-
 --
 -- copyright (c) 2003 Shae Erisson
 -- license: lGPL
 -- quick ugly hack to get factoids in lambdabot
 -- requires Database.PostgreSQL.HSQL from
 -- http://sf.net/projects/htoolkit/
--- 
--- assumes a postgresql database with a table named fact and fields named fact and value
+--
+-- This is a rewrite of Shaes originial code to use internal module
+-- states. jlouis@.
 --
 
 module FactModule (theModule) where
 
 import IRC
 import Util
-
-import Data.List
-import Data.Char
-import Control.Monad.Reader
-import Database.PostgreSQL.HSQL
+import qualified Map as M
 
 newtype FactModule = FactModule ()
 
-theModule = MODULE factModule
-factModule = FactModule ()
+theModule :: MODULE
+theModule = MODULE $ FactModule()
 
-instance Module FactModule where
-    commands     m = return ["fact"]
-    process      m _ target cmd rest
-                 = do -- thanks Marvin--
-                   x <- liftIO (factoid rest)
-                   ircPrivmsg target x
+type FactState = M.Map String String
+-- type Fact m a = ModuleT FactState m a
 
--- note the handy error catching, now with 15% less crashes!
-factoid                     :: String -> IO String
-factoid rest
- | length (words rest) > 1  = if (factKey == "delete") then catchSql (factDel factVal) sqlHandler
-                              else catchSql (factSet factKey factVal) sqlHandler
- | length (words rest) == 1 =  catchSql (factGet factKey) sqlHandler
- | otherwise                = return "empty factoid, BZZZT, thank you for playing!"
-    where
-    factKey = lowerCaseString $ clean $ head $ words rest
-    factVal = clean $ snd $ break (== ' ') rest
+instance Module FactModule FactState where
+  moduleHelp _ "fact" = return "Retrieve a fact from the database"
+  moduleHelp _ "fact-set" = return "Define a new fact"
+  moduleHelp _ _ = return "Store and retrieve facts from a database"
 
--- postgresql stuff under here
-dbhost = "localhost"
-username = "shae"
-database = "shae"
-password = "password"
+  moduleDefState _  = return $ M.empty
+  moduleSerialize _ = Just mapSerializer
 
-connectGet = connect dbhost username database password
+  moduleCmds   _ = return ["fact", "fact-set"]
 
-sqlHandler se = do putStrLn (show se)
-                   return (show se)
-
-getFVV :: Statement -> IO [Char]
-getFVV = flip getFieldValue "value"
-
-factGet   :: [Char] -> IO [Char]
-factGet x =  do connection <- connectGet
-                statement  <- query connection (fact_get_query x)
-                results <- liftM concat $ liftM (intersperse ", ") $ collectRows getFVV statement
-                disconnect connection
-                if (results == "") then return "Nothing" else return results
-
-factSet :: [Char] -> [Char] -> IO [Char]
-factSet x y = do connection <- connectGet
-                 execute connection $ fact_set_query x y
-                 disconnect connection
-                 return ("set " ++ x ++ " to " ++ y)
-
-factDel   :: [Char] -> IO [Char]
-factDel x = do connection <- connectGet
-               execute connection $ fact_del_query x
-               disconnect connection
-               return ("deleted " ++ x)
-
-fact_get_query   :: [Char] -> [Char]
-fact_get_query x =  "select * from fact where fact = '" ++ x ++ "';"
-
-fact_del_query   :: [Char] -> [Char]
-fact_del_query x = "delete from fact where fact = '" ++ x ++ "';"
-
-fact_set_query :: [Char] -> [Char] -> [Char]
-fact_set_query x y = concat ["INSERT INTO fact ( fact, value ) VALUES ('",x,"','",y,"');"]
+  process _ _ target cmd rest =
+    do factFM <- readMS
+       result <- case words rest of
+                   [] -> return "I can not handle empty facts."
+		   (fact:dat) -> case cmd of
+                                   "fact" -> return $ getFact factFM fact
+                                   "fact-set" -> do writeMS $ 
+						      M.insert fact 
+							       (unwords dat)
+							       factFM
+						    return $
+                                                      "Fact recorded."
+		                   _ -> return "Unknown command."
+       ircPrivmsg target result
 
 
-
-clean = unticker . trim
--- single tick escaping code from Igloo
-unticker str = concatMap untick str
-
--- extra escaping code from tmoertel
-untick x | x `elem` specials = ['\\',x]
-         | otherwise         = [x]
-         where
-         specials = "\\\'"
--- trim whitespace from the front and back of the string
-trim str = reverse $ dropS $ reverse $ dropS str
-           where dropS = dropWhile isSpace
+getFact :: M.Map String String -> String -> String
+getFact fm fact =
+  case M.lookup fact fm of
+    Nothing -> "I know nothing about " ++ fact ++ "."
+    Just x  -> fact ++ ": " ++ x ++ "."
