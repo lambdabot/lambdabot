e3abbae31fc69ed971b5f36aeb5215685275d1ad
Don Stewart <dons@cse.unsw.edu.au>
2005-04-14 01:50:22 +0000

    Have @help return a list of commands provided by a module, if the module has no default help

--- lambdabot/HelpModule.hs
+++ lambdabot/HelpModule.hs
@@ -1,8 +1,12 @@
+--
+-- | Provide help for plugins
+--
 module HelpModule (theModule) where
 
 import IRC
-import Control.Exception   (Exception(..))
-import Control.Monad.Error (catchError, throwError)
+import Util                 (showClean)
+import Control.Exception    (Exception(..))
+import Control.Monad.Error  (catchError, throwError)
 
 newtype HelpModule = HelpModule ()
 
@@ -18,11 +22,27 @@ doHelp :: HelpModule -> IRCMessage -> String -> String -> [Char] -> ModuleT () I
 
 doHelp m msg target cmd "" = doHelp m msg target cmd "help"
 
-doHelp m msg target cmd rest = withModule ircCommands arg
-    (process m msg target cmd "help") {- then default msg -} (\md -> do
-      helpString <- catchError (liftLB $ moduleHelp md arg) $ \e ->
-        case e of        -- module doesn't define moduleHelp
-            IRCRaised (NoMethodError _) -> return "no help for this command"
-            _ -> throwError e
-      ircPrivmsg target helpString)
-  where (arg:_) = words rest
+--
+-- If a target is a command, find the associated help, otherwise if it's
+-- a module, return a list of commands that module implements.
+--
+doHelp m msg target cmd rest = 
+    withModule ircCommands arg                  -- see if it is a command
+        (withModule ircModules arg              -- else maybe it's a module name
+            (process m msg target cmd "help")   -- else give up
+            -- its a module
+            (\md -> do
+                ss <- liftLB $ moduleCmds md
+                let s | null ss   = arg ++ " is a module."
+                      | otherwise = arg ++ " provides: " ++ (showClean ss)
+                ircPrivmsg target s))
+
+        -- so it's a valid command, try to find its help
+        (\md -> do
+            s <- catchError (liftLB $ moduleHelp md arg) $ \e ->
+                case e of
+                    IRCRaised (NoMethodError _) -> return "This command is unknown."
+                    _                           -> throwError e
+            ircPrivmsg target s)
+
+    where (arg:_) = words rest
