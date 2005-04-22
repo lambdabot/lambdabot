2f59106c71f3446db00b973e5e8336cf7918d5bc
dons <dons@cse.unsw.edu.au>
2005-04-22 02:47:38 +0000

    Don't need Depends type now

--- Config.hs
+++ Config.hs
@@ -44,18 +44,3 @@ config = Config {
                 "TheHunter",    "jlouis"
         ]
    }
-
---
--- | This type describes object dependencies required at runtime.
--- It is calculated at build time with 'make Depends.conf', and used
--- by the Dynamic module, and GenModules
---
-data Depends = Depends {
-        reqObjs :: [String],           -- ^ a common subset of modules 
-        reqPkgs :: [String],           -- ^ all required packages
-        depList :: [(String,[String])] -- ^ assoc list of modules and their obj deps
-    }
-    deriving (Show,Read)
-
--- | An association between modules and their dependencies
-type DepList = [(String,[String])]
