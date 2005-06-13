00f5483b130c208462574a8de2bd0f881fd383a8
Don Stewart <dons@cse.unsw.edu.au>
2005-06-13 12:24:27 +0000

    new topic code

--- Plugins/Topic.hs
+++ Plugins/Topic.hs
@@ -15,26 +15,31 @@ theModule :: MODULE
 theModule = MODULE $ TopicModule ()
 
 instance Module TopicModule () where
-  moduleHelp _ _ = return "Various commands for adjusting the channel topic"
+  moduleHelp _ _ = return "Various commands for adjusting the channel topic\nUsage: @topic-XXX #chan <string>"
   moduleCmds   _ = return ["topic-tell",
                            "topic-cons", "topic-snoc",
-                           "topic-tail", "topic-init"]
+                           "topic-tail", "topic-init", "topic-null"]
+
   process _ _ src "topic-cons" text = topicSplit (:) src text
   process _ _ src "topic-snoc" text = topicSplit snoc src text
   process _ _ src "topic-tail" chan = alterTopic src chan tail
   process _ _ src "topic-init" chan = alterTopic src chan init
+  process _ _ _   "topic-null" chan = ircTopic chan "[]"
+
   process _ _ src "topic-tell" chan =
       lookupTopic chan (\maybetopic ->
         case maybetopic of
-	  Just x -> ircPrivmsg src x
+	  Just x  -> ircPrivmsg src x
 	  Nothing -> ircPrivmsg src "do not know that channel")
+
   process _ _ src cmd _
     = ircPrivmsg src ("Bug! someone forgot the handler for \""++cmd++"\"")
 
-topicSplit :: (String -> [String] -> [String])
-	       -> String -> String -> IRC ()
+
+topicSplit :: (String -> [String] -> [String]) -> String -> String -> IRC ()
 topicSplit f source cmdtext = alterTopic source chan (f topic_item)
-  where (chan, topic_item) = splitFirstWord cmdtext
+  where 
+      (chan, topic_item) = splitFirstWord cmdtext
 
 lookupTopic :: String -> (Maybe String -> IRC () ) -> IRC ()
 lookupTopic chan f =
@@ -46,11 +51,14 @@ alterTopic source chan f =
   let p maybetopic =
         case maybetopic of
           Just x -> case reads x of
-                        [(xs, "")] -> ircTopic chan (show $ f $ xs)
-			[(xs, r)] | length r <= 2 
-				   -> do ircPrivmsg source $
-				              "ignoring bogus characters: " ++ r
-					 ircTopic chan (show $ f $ xs)
-			_          -> ircPrivmsg source "topic does not parse"
-	  Nothing -> ircPrivmsg source ("I do not know the channel " ++ chan)
-  in lookupTopic chan p
+                [(xs, "")] -> ircTopic chan (show $ f $ xs)
+                [(xs, r)] | length r <= 2 
+                  -> do ircPrivmsg source $ "ignoring bogus characters: " ++ r
+                        ircTopic chan (show $ f $ xs)
+
+                _ -> ircPrivmsg source 
+                         "topic does not parse. topic should be of the form [\"...\",...,\"...\"]"
+
+          Nothing -> ircPrivmsg source ("I do not know the channel " ++ chan)
+
+        in lookupTopic chan p
