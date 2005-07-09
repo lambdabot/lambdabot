4990e369796051d1426cfb0b7905592a2274810c
dons <dons@cse.unsw.edu.au>
2005-07-09 03:16:06 +0000

    Improved @hoogle

--- Plugins/Hoogle.hs
+++ Plugins/Hoogle.hs
@@ -2,6 +2,9 @@ module Plugins.Hoogle (theModule) where
 
 import Lambdabot
 import PosixCompat
+import LBState
+
+import Control.Monad	   ( when )
 import Control.Monad.Trans ( liftIO )
 
 newtype HoogleModule = HoogleModule ()
@@ -9,23 +12,38 @@ newtype HoogleModule = HoogleModule ()
 theModule :: MODULE
 theModule = MODULE $ HoogleModule ()
 
-instance Module HoogleModule () where
-    moduleSticky _ = False
+type HoogleState = [String]
+
+instance Module HoogleModule HoogleState where
+    moduleSticky   _ = False
+    moduleDefState _ = return []
 
     moduleHelp _ _ = return "@hoogle <expr>, Haskell API Search for either names, or types."
+    moduleCmds   _ = return ["hoogle", "hoogle+"]
 
-    moduleCmds   _ = return ["hoogle"]
+    process _ _ src "hoogle" s = do 
+	o <- liftIO $ hoogle s
+	let (this,that) = splitAt 3 o
+	writeMS that
+	ircPrivmsg src (unlines this)
 
-    process _ _ src "hoogle" s = do o <- liftIO $ hoogle s
-                                    ircPrivmsg src o
+    process  _ _ src "hoogle+" _ = do
+	this <- withMS $ \st write -> do
+			let (this,that) = splitAt 3 st
+			write that
+			return this
+	when (not . null $ this) $
+		ircPrivmsg src (unlines this)
+
     process _ _ _ _ _ = error "HoogleModule: invalid command"
 
 binary :: String
-binary = "/home/paolo/code/haskell/hoogle/hoogle"
+binary = "/home/dons/bin/hoogle"
 
-hoogle :: String -> IO String
+hoogle :: String -> IO [String]
 hoogle src = do (out,err,_) <- popen binary [src] (Just "")
                 return $ result out err
-    where result [] [] = "Terminated\n"
-          result [] ys = ys
-          result xs _  = takeWhile (/='\n') xs
+    where result [] [] = ["Terminated\n"]
+          result [] ys = [ys]
+          result xs _  = lines xs
+
