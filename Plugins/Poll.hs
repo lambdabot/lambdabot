e8199eac767af8e7042f554d5375d94ed9252a23
dons <dons@cse.unsw.edu.au>
2005-11-24 00:04:01 +0000

    Name the @poll-* commands in a consistent manner

--- Plugins/Poll.hs
+++ Plugins/Poll.hs
@@ -7,7 +7,7 @@
 -- | added by Kenneth Hoste (boegel), 22/11/2005
 -- |  inspiration: Where plugin (thanks shapr,dons)
 --
-module Plugins.Vote (theModule) where
+module Plugins.Poll (theModule) where
 
 import Lambdabot
 import LBState
@@ -64,35 +64,38 @@ instance Packable (M.Map P.FastString Poll) where
 ------------------------------------------------------------------------
 
 instance Module VoteModule VoteState where
-    moduleCmds     _ = return ["browse-polls"
-                              ,"show-poll"
-                              ,"add-poll"
-                              ,"add-choice"
+    moduleCmds     _ = return ["poll-browse"
+                              ,"poll-show"
+                              ,"poll-add"
+                              ,"choice-add"
                               ,"vote"
-                              ,"show-result"
-                              ,"close-poll"
-                              ,"remove-poll"]
+                              ,"poll-result"
+                              ,"poll-close"
+                              ,"poll-remove"]
+--
     
     moduleHelp   _ s = return $ case s of
-        "browse-polls"  -> "@browse-polls, shows all current polls"
-        "show-poll"     -> "@show-poll <poll>, shows all choices for some poll"
-        "add-poll"      -> "@add-poll <name> <choice>, adds a new poll with a single choice"
-        "add-choice"    -> "@add-choice <poll> <choice>, adds a new choice to the given poll"
-        "vote"          -> "@vote <poll> <choice>, use this for voting on <choice> in given poll"
-        "show-result"   -> "@show-result <poll>, shows result for given poll"
-        "close-poll"    -> "@close-poll <poll>, closes a poll"
-        "remove-poll"   -> "@remove-poll <poll>, removes a poll"
-        _               -> "vote plugin - allows to cast a vote on some subject"
+        "poll-browse"   -> "@poll-browse                 shows all current polls"
+        "poll-show"     -> "@poll-show <poll>            shows all choices for some poll"
+        "poll-add"      -> "@poll-add <name> <choice>    adds a new poll with a single choice"
+        "choice-add"    -> "@choice-add <poll> <choice>  adds a new choice to the given poll"
+        "vote"          -> "@vote <poll> <choice>        vote for <choice> in <poll>"
+        "poll-result"   -> "@poll-result <poll>          show result for given poll"
+        "poll-close"    -> "@poll-close <poll>           closes a poll"
+        "poll-remove"   -> "@poll-remove <poll>          removes a poll"
+        _               -> "vote plugin - run elections on various topics"
     
     moduleDefState _  = return M.empty
     moduleSerialize _ = Just voteSerial
     
-    process _ _ target "browse-polls" _ = do 
-        result <- withMS $ \factFM writer -> processCommand factFM writer "browse-polls" []
+    process _ _ target "poll-browse" _ = do 
+        result <- withMS $ \factFM writer -> processCommand factFM writer "poll-browse" []
         ircPrivmsg target result
+
     process _ _ target _ [] = do 
         result <- return  "Don't forget the parameters ! Check @help <vote-cmd> for info."
         ircPrivmsg target result
+
     process _ _ target cmd dat = do
         result <- withMS $ \factFM writer -> processCommand factFM writer cmd (words dat)
         ircPrivmsg target result
@@ -100,35 +103,35 @@ instance Module VoteModule VoteState where
 processCommand :: VoteState -> VoteWriter -> String -> [String] -> Vote LB String
 
 processCommand fm writer cmd dat = case cmd of
-    "browse-polls"  -> return $ browsePolls fm
+    "poll-browse"  -> return $ browsePolls fm
 
-    "show-poll"     -> return $ if length dat >= 1 
+    "poll-show"    -> return $ if length dat >= 1 
                         then showPoll fm (head dat) 
-                        else "usage: @show-poll <poll>"
+                        else "usage: @poll-show <poll>"
 
-    "add-poll"      -> if length dat >= 2
+    "poll-add"     -> if length dat >= 2
                         then addPoll fm writer (head dat) (dat !! 1) 
-                        else return "usage: @add-poll <poll> <first choice>"
+                        else return "usage: @poll-add <poll> <choice>"
 
-    "add-choice"    -> if length dat >=2 
+    "choice-add"   -> if length dat >=2 
                         then addChoice fm writer (head dat) (dat !! 1)
-                        else return "usage: @add-choice <poll> <choice>"
+                        else return "usage: @choice-add <poll> <choice>"
 
     "vote"          -> if length dat >= 2 
                         then vote fm writer (head dat) (dat !! 1)
                         else return "usage: @vote <poll> <choice>"
 
-    "show-result"   -> return $ if length dat >= 1
+    "poll-result"   -> return $ if length dat >= 1
                         then showResult fm (head dat)
-                        else "usage: @show-result <poll"
+                        else "usage: @poll-result <poll>"
 
-    "close-poll"    -> if length dat >= 1 
+    "poll-close"    -> if length dat >= 1 
                         then closePoll fm writer (head dat)
-                        else return "usage: @close-poll <poll>"
+                        else return "usage: @poll-close <poll>"
 
-    "remove-poll"   -> if length dat >= 1 
+    "poll-remove"   -> if length dat >= 1 
                         then removePoll fm writer (head dat)
-                        else return "usage: remove-poll <poll>"
+                        else return "usage: @poll-remove <poll>"
 
     _ -> return "Unknown command."
 
@@ -140,7 +143,7 @@ browsePolls fm = show $ map fst (M.toList fm)
 showPoll :: VoteState -> String -> String
 showPoll fm poll = 
     case M.lookup (P.pack poll) fm of
-        Nothing -> "No such poll ! Use @browse-polls to see the available polls."
+        Nothing -> "No such poll ! Use @poll-browse to see the available polls."
         Just p  -> show $ map fst (snd p)
 
 addPoll :: VoteState -> VoteWriter -> String -> String -> Vote LB String
