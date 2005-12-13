0b89746e486ddddd68c8382f30e801801a60edb5
softpro <softpro@gmx.net>
2005-12-13 19:05:54 +0000

    fixed issue with lambdabot's indentation filtering

--- Plugins/Pretty.hs
+++ Plugins/Pretty.hs
@@ -17,9 +17,10 @@ module Plugins.Pretty where
 
 import Lambdabot
 import Language.Haskell.Parser (parseModule, ParseResult(..))
-import Language.Haskell.Syntax (SrcLoc(..), HsModule(..))
-import Language.Haskell.Pretty (prettyPrintWithMode, defaultMode)
-  -- PPHsMode(..)), PPLayout(..))
+import Language.Haskell.Syntax 
+    (HsMatch(..), HsDecl(..), HsName(..), HsPat(..), SrcLoc(..), HsModule(..))
+import Language.Haskell.Pretty 
+    (prettyPrintWithMode, defaultMode, PPHsMode(..))
 import List (intersperse, isPrefixOf)
 
 ------------------------------------------------------------------------
@@ -36,16 +37,32 @@ instance Module PrettyModule (String -> IO String) where
         "pretty" -> prettyCmd src rest
         _        -> error "unknown command"
 
+-- | calculates "desired" indentation and return pretty-printed declarations
+-- the indentation calculations are still pretty much rough guesswork.
+-- i'll have to figure out a way to do some _reliable_ pretty-printing!
 doPretty :: HsModule -> String
 doPretty (HsModule _ _ _ _ decls) =
-    let ppMode = defaultMode {-
-        doIndent      = 2,
-        caseIndent    = 2,
-        letIndent     = 2,
-        whereIndent   = 2,
-        onsideIndent  = 2,
-    -}
-    in concat . intersperse "\n" . map (prettyPrintWithMode ppMode) $ decls
+    let defaultLen = 4
+        declLen (HsFunBind matches)   = maximum $ map matchLen matches
+        declLen (HsPatBind _ pat _ _) = patLen pat
+        declLen _  = defaultLen
+        patLen (HsPVar name) = nameLen name
+        patLen  _  = defaultLen
+        nameLen (HsIdent s)  = length s + 1
+        nameLen _  = defaultLen
+        matchLen (HsMatch _ name pats _ _) = 
+            let l = (nameLen name + sum (map patLen pats) + 1)
+            in if l > 16 then defaultLen else l
+        makeMode decl = defaultMode {
+            doIndent     = 3,
+            caseIndent   = 4,
+            onsideIndent = declLen decl
+        }
+    -- FIXME: prefixing with hashes is done, because i didn't find a way
+    --   to disable the indentation filter of lambdabot only for this module...
+    in unlines . map ("#  "++) . lines . concat . intersperse "\n" 
+       -- . map show $ decls
+       . map (\d -> prettyPrintWithMode (makeMode d) d) $ decls
 
 
 -- FIXME: the solution of prefixing code with a module prelude
