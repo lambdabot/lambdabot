8acd3f8e10cd37059c6536c12c4ecd92506aac09
tatd2 <tatd2@kent.ac.uk>
2006-02-21 23:56:01 +0000

    Slapping Module

---
+++ Plugins/Slap.hs
@@ -0,0 +1,42 @@
+--
+-- | Support for quotes
+--
+module Plugins.Slap (theModule) where
+
+import Lambdabot
+import qualified IRC
+import Util                     (stdGetRandItem)
+
+import Control.Monad.Trans      (liftIO)
+import Control.Monad            (ap)
+
+------------------------------------------------------------------------
+newtype QuoteModule = QuoteModule ()
+
+theModule :: MODULE
+theModule = MODULE $ QuoteModule ()
+
+instance Module QuoteModule () where
+    moduleCmds           _ = ["slap"]
+    moduleHelp _ "slap"    = "Slap someone ammusingly"
+
+    process _ msg _ cmd rest = do
+       quote <- liftIO $ case cmd of
+                  "slap"  -> slapRandom (if rest == "me" then sender else rest)
+                  _ -> error "QuoteModule: bad string"
+       return [quote]
+       where sender = IRC.nick msg
+
+------------------------------------------------------------------------
+
+-- | Return a random arr-quote
+slapRandom :: String -> IO String
+slapRandom x = ap (Util.stdGetRandItem slapList) (return x)
+
+slapList :: [String -> String]
+slapList = 
+    [("/me slaps " ++)
+    ,(\x -> "/me smacks " ++ x ++ " about with a large trout")
+    ,("/me beats up " ++)
+    ,("why on earth would I slap " ++)
+    ]
