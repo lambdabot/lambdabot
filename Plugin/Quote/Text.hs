13a2515c1db12f190c6be54314a057b1e000a2ad
dons <dons@cse.unsw.edu.au>
2006-04-06 10:39:35 +0000

    Add @brain

---
+++ Plugin/Quote/Text.hs
@@ -0,0 +1,218 @@
+--
+-- | Some quote text
+--
+module Plugin.Quote.Text where
+
+-- | Some pirate quotes
+arrList :: [String]
+arrList =
+    ["Avast!"
+    ,"Shiver me timbers!"
+    ,"Yeh scurvy dog..."
+    ,"I heard andersca is a pirate"
+    ,"I'll keel haul ya fer that!"
+    ,"I'd like to drop me anchor in her lagoon"
+    ,"Well me 'earties, let's see what crawled out of the bung hole..."
+    ,"I want me grog!"
+    ,"Drink up, me 'earties"
+    ,"Is that a hornpipe in yer pocket, or arr ya just happy ta see me?"
+    ,"Get out of me way, yeh landlubber"
+    ,"Smartly me lass"
+    ,"Arrr!"
+    ,"Ahoy mateys"
+    ,"Aye"
+    ,"Aye Aye Cap'n"
+    ,"This is the END for you, you gutter-crawling cur!"
+    ,"May the clap make ye incapable of Cracking Jenny's Tea Cup."
+    ,"Eat maggoty hardtack, ye unkempt, jenny frequentin', son of a gun."
+    ,"Swab the deck!"
+    ,"Keelhaul the swabs!"
+    ,"Yo ho ho, and a bottle of rum!"
+    ,"I'll crush ye barnacles!"
+    ,"Har de har har!"
+    ]
+
+--
+-- Actual quotes from an asshat called Keal over Jan 12-14 2006.
+--
+kealList :: [String]
+kealList =
+    ["endian mirrors the decimal"
+    ,"primary elemental assumption of integer coefficients to roots in counting sytem is wrong"
+    ,"actually it bug in math"
+    ,"b*(Floor[v/b^p]/b-Floor[Floor[v/b^p]/b])"
+    ,"proofs are no longer sound"
+    ,"my proof show math is broken right now"
+    ,"doctor just give meds not fix prollem"
+    ,"Keal was so happy with T, coded in basic so run on anything, and does lot"
+    ,"one prollem. T broke confines of the visual basic langage and would not compile"
+    ,"perhaps i just genius and never tested"
+    ,"and yes that was with zero formal training in all realms"
+    ,"somone would expect that trees 500gb hdds of expressions as if they were floppy dicks"
+    ,"can you make a macro that builds the expression accoridng to a genetic algorithm where you decide what is good and what is bad?"
+    ,"T could perform expressions 600mb and bigger"
+    ,"what is the max amount of operands haskell can handle in a single expression?"
+    ,"T seems to be haskell, except with a decent interface at this point"
+    ,"love a black and white lower 128 from 32 up of ascii glyphs?"
+    ,"evaluating expressions is ALL haskell does?????"
+    ,"you think i am one of them persnipity uppity men are pig lesbian mathematicians?"
+    ,"how bout i say ick no unicorn and daisy loving girl mathematician will ever enjoy this"
+    ,"better be atleast 16x16 color with extended ascii set"
+    ,"what the hell does Prelude > mean?"
+    ,"how do i search for someone saying 'Keal' in mirc"
+    ,"i have basically written a proof that shows an assumption is wrong"
+    ,"they dumbified you"
+    ,"antiparsimony were 100% correct..."
+    ,"its because the timeline diverges and past events themselves unhappen"
+    ,"all i know is i have experienced my own death unhappening..."
+    ," what have you been smoking? you narrow minded Haskell user?"
+    ,"i use an 8088"
+    ,"my very first computer was an 80-0840"
+    ,"it is very easy to go off topic"
+    ,"someone needs to write a boids for haskell that emulates humans going on and off topic"
+    ,"i just got banned from math because i not have good ability to convey thoughts"
+    ,"i lack in verbal and social expression"
+    ,"i try make program called Glyph to do it but my script lang called T too slow. i invent T"
+    ,"can GMP support KealDigit? I invent KealDigit"
+    ,"with KealDigit quantum crackproof encryption possible"
+    ,"i show how spell triangle in less than three corners using darkmanifold"
+    ,"can haskell pipe the raw irrational megaequation into an analog device"
+    ,"the fractal is 5 irrationals"
+    ,"99% of my book has been erased by faulty hdd's"
+    ,"last day i was in my lab i had a diagram which might have removed pi"
+    ,"i only trust opensource tools. where can i download haskell for windows?"
+    ,"obviously you never heard of Tier. theoretically it would work using nanobots"
+    ,"you need a Zh function in Haskell"
+    ,"can haskell compile flash animations and java apps?"
+    ,"i need math friendly compiler to compile for jvm or flash"
+    ,"Cale etc already pointed out Haskell is puny to nothing to emulate using my barrage of mathematic theories"
+    ,"i prove infinity never ends in both directions"
+    ,"are you saying i am MegaMonad?"
+    ,"Keal angry @ dons"
+    ,"i can explain why something is without knowing what the rules decided by man are"
+    ,"making a bot of me is highly offensive"
+    ,"just seeing how offtopic i could get everyone"
+    ,"intuitive != imperative"
+    ,"doubles and floats cause b*(Floor[v/b^p]/b-Floor[Floor[v/b^p]/b) to fuck up"
+    ,"what are epsilons?"
+    ,"haskell always said undefined"
+    ,"bot seems useless"
+    ,"when i put what i dat recoved from that tile into a ti92. the damn thing blew up"
+    ,"i think it because mathematics damage you cpu"
+    ,"ithink has to do with hardcased government failsafe in chip"
+    ,"i suggest you tear apart a 20q and plug it with the alg"
+    ,"nsa has all the profiling info you need to come up with the correct survey answers"
+    ,"write an algorthim that generates the correct responses for a phone survey based on number of rings whether answered how quickly hung up on and the mood of the receiver"
+    ,"where can i find opensource schematics of Linus Torvalds' x86 clone?"
+    ,"need to plan a fieldtrip to Frederick B. Mancoff of Freescale Semiconductor"
+    ,"ghc need to have plugin that allow copy paste in xp"
+    ,"know you know this 24 periods Keal SecretTM"
+    ,"tomorrow i share next mathematical secrety"
+    ,"nsa prevent me from returning to math on efnet"
+    ,"nsa try kill me numerous times"
+    ,"#haskell needs to take its meds"
+    ,"i think i know what code does but code looks to simple to actually do it"
+    ,"need 1 to do a while 0 does !a. need 1 to do a while 0 does !a"
+    ,"will it return [] if map gives fpu infinite list?"
+    ,"today's 24hour project was supposed to be logical overloading using plegm method"
+    ,"there is no way to prove the failsafe exists"
+    ,"oh btw my fpu is blown due to a hardcased failsafe i have 3 year warranty right. and then they call fads"
+    ,"i aint running that on my puter"
+    ,"lamadabot took 5 to 8 whole seconds to return []"
+    ,"bot defective"
+    ,"i changed my user od"
+    ,"i cant think anymore"
+    ,"i want to invent white dye"
+    ,"pork steaks taste like dick"
+    ,"i dont really eat vegetables unless cheese is a vegetable"
+    ,"the [nsa] even make light green both ways once"
+    ]
+
+--
+-- Quotes from the lyrics of B52s songs. They remind me (dons) of zippy.
+-- 
+b52s :: [String]
+b52s =
+    [ "His ear lobe fell in the deep. Someone reached in and grabbed it. It was a rock lobster!"
+    , "Watch out for that piranha. There goes a narwhale. HERE COMES A BIKINI WHALE!"
+    , "She drove a Plymouth Satellite faster than the speed of light!"
+    , "Some say she's from Mars, or one of the seven stars that shine after 3:30 in the morning. WELL SHE ISN'T."
+    , "It's a dreary downtown day, but at the end of my 40 foot leash is my little friend quiche."
+    , "Girl from Ipanema, she goes to Greenland"
+    , "Hot pants explosion at the factory!"
+    , "You belong in Ripley's Believe It Or Not"
+    ]
+
+--
+-- Quotes from the pinky and the brain cartoon
+--
+brain :: [String]
+brain =
+    [("Gee, Brain, what are we going to do tonight?\n"  ++
+      "The same thing we do every night, Pinky. Try to take over the world!")
+    , "Narf!"
+    , "Zort!"
+    , "Poit!"
+    , "Troz!"
+    , "It must be inordinately taxing to be such a boob."
+    , "Promise me something, Pinky. Never breed."
+    , "Pinky, I am in considerable pain."
+    , "Here we are, Pinky--at the dawn of time!"
+    ,("Now, Pinky, if by any chance you are captured during this mission, \n" ++
+      "remember you are Gunther Heindriksen from Appenzell. You moved to Grindelwald\n" ++
+      "to drive the cog train to Murren.  Can you repeat that?")
+    ,("Brain! Brain! You aren't going to leave me!!!???" ++
+      "You know what happened to Jerry Lewis after Dean Martin left him!!!")
+    , "They've turned into giant Swiss leaderhosen-clad dancing yodelers. Talk about unpredictable!"
+    , "Be quiet Pinky, or I shall have to hurt you."
+    , "If I could reach you I would hurt you."
+    ,("What can I do for fun, Pinky? That's it! I'll send several\n" ++
+      "bills to Senate for ratification, then veto them all!")
+    , "There's only one ride that interests me - the incredible thrill ride of taking over the world!"
+    ,("It is here that my cheap workforce of trained iguanas will work\n" ++
+      "night and day to make our shoes to my exacting specifications!")
+    ,("Has it ever occurred to you, Pinklet, that your scarf is\n" ++
+      "constricting the bloodflow to your head?")
+    , "Are you pondering what I'm pondering?"
+    , "But where are we going to find a duck and a hose at this hour?"
+    , "But where will we find an open tattoo parlor at this time of night?"
+    , "Uh... yeah, Brain, but where are we going to find rubber pants our size?"
+    , "Uh, I think so, Brain, but balancing a family and a career ... ooh, it's all too much for me."
+    , "Wuh, I think so, Brain, but isn't Regis Philbin already married?"
+    , "Wuh, I think so, Brain, but burlap chafes me so."
+    , "Uh, I think so, Brain, but we'll never get a monkey to use dental floss."
+    , "Uh, I think so Brain, but this time, you wear the tutu."
+    , "I think so, Brain, but culottes have a tendency to ride up so."
+    , "I think so, Brain, but if they called them 'Sad Meals', kids wouldn't buy them!"
+    , "I think so, Brain, but me and Pippi Longstocking -- I mean, what would the children look like?"
+    , "Well, I think so, Brain, but I can't memorize a whole opera in Yiddish."
+    , "I think so, Brain, but there's still a bug stuck in here from last time."
+    , "Uh, I think so, Brain, but I get all clammy inside the tent."
+    , "I think so, Brain, but I don't think Kaye Ballard's in the union."
+    , "I think so, Brain, but, the Rockettes? I mean, it's mostly girls, isn't it?"
+    , "I think so, Brain, but pants with horizontal stripes make me look chubby."
+    , "Well, I think so, Brain, but pantyhose are so uncomfortable in the summertime."
+    , "Well, I think so, Brain, but it's a miracle that this one grew back."
+    , "Well, I think so, Brain, but first you'd have to take that whole bridge apart, wouldn't you?"
+    , "Well, I think so, Brain, but 'apply North Pole' to what?"
+    , "Umm, I think so, Brain, but what if the chicken won't wear the nylons?"
+    , "I think so, Brain, but isn't that why they invented tube socks?"
+    , "Well, I think so Brain, but what if we stick to the seat covers?"
+    , "Oooh, I think so Brain, but I think I'd rather eat the Macarena."
+    , "Well, I think so, but Kevin Costner with an English accent?"
+    , "I think so, Brain, but don't you need a swimming pool to play Marco Polo?"
+    , "Well, I think so, Brain, but do I really need two tongues?"
+    , "I think so, Brain, but we're already naked."
+    , "I think so, Brain, but Lederhosen won't stretch that far."
+    , "Yeah, but I thought Madonna already had a steady bloke!"
+    , "I think so, Brain?but how would we ever determine Sandra Bullock's shoe size?"
+    , "I think so, Brain, but a codpiece made from a real fish would get smelly after a while, wouldn?t it?"
+    , "I think so, Commander Brain from Outer Space! But do we have time to grease the rockets?"
+    , "I think so, Doctor. But are these really the legs of a show girl?"
+    , "Whuu... I think so, BrainPan! But if running shoes had little feet, wouldn't they need their own shoes?"
+    , "I think so, Brain! But ruby-studded stockings would be mighty uncomfortable wouldn't they?"
+    , "I think so, Brain! How much deeper would the ocean be if there weren't sponges down there?"
+    , "I think so, Brain! But do I have what it take to be the 'Lord of the Dance'?"
+    , "I think so, Brain, but I didn?t know 90210 was a real zip code! Will Tori be there?"
+    , "The game does not conclude until the woman with the eating disorder ululates."
+    ]
