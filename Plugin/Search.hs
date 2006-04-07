edd3441a3cee9ffde47821f511b8aa96f4b4e30c
dons <dons@cse.unsw.edu.au>
2006-04-07 06:20:55 +0000

    @gwiki searches the haskell wiki using google

--- Plugin/Search.hs
+++ Plugin/Search.hs
@@ -2,6 +2,7 @@
 -- | Search various things, Wikipedia and google for now.
 --
 -- (c) 2005 Samuel Bronson
+-- (c) 2006 Don Stewart
 --
 -- Joel Koerwer 11-01-2005 generalized query for different methods
 --   and added extractConversion to make things like @google 1+2 work
@@ -17,23 +18,26 @@ theModule :: MODULE
 theModule = MODULE $ SearchModule ()
 
 engines :: [(String, (String, String))]
-engines =  [("google"
-           ,("http://www.google.com/search?hl=en&q=","&btnI=I%27m+Feeling+Lucky")),
+engines =
+   [("google"
+   ,("http://www.google.com/search?hl=en&q=","&btnI=I%27m+Feeling+Lucky")),
 
-            ("wikipedia"
-           ,("http://en.wikipedia.org/wiki/Special:Search?search=", "")),
+    ("wikipedia"
+   ,("http://en.wikipedia.org/wiki/Special:Search?search=", "")),
 
-            ("gsite"
-           ,("http://www.google.ca/search?hl=en&q=site%3A", "&btnI=I%27m+Feeling+Lucky"))
-           ]
+    ("gsite"
+   ,("http://www.google.ca/search?hl=en&q=site%3A", "&btnI=I%27m+Feeling+Lucky"))
+   ]
 
 instance Module SearchModule () where
     moduleHelp _ s      = case s of
          "google"    -> "google <expr>. Search google and show url of first hit"
          "wikipedia" -> "wikipedia <expr>. Search wikipedia and show url of first hit"
          "gsite"     -> "gsite <site> <expr>. Search <site> for <expr> using google"
-    moduleCmds      _   = map fst engines
-    process_ _          = (. dropSpace) . searchCmd
+         "gwiki"     -> "wiki <expr>. Search (new) haskell.org wiki for <expr> using google."
+    moduleCmds      _   = "wiki" : map fst engines
+    process_ _ "gwiki" e = ((. dropSpace) . searchCmd) "gsite" ("haskell.org/haskellwiki/" ++ e)
+    process_ _ s      e = ((. dropSpace) . searchCmd) s e
 
 ------------------------------------------------------------------------
 
@@ -60,7 +64,6 @@ queryit meth engine q = readPage (proxy config) uri request ""
                         Nothing -> [meth ++ " " ++ abs_path ++ " HTTP/1.0", ""]
                         _       -> [meth ++ " " ++ url ++ " HTTP/1.0", ""]
 
-
 extractLoc :: [String] -> Maybe String
 extractLoc [] = error "No response, something weird is up."
 extractLoc (_:headers) = lookup "Location" $ concatMap f headers
@@ -70,39 +73,20 @@ extractLoc (_:headers) = lookup "Location" $ concatMap f headers
 
 extractConversion :: [String] -> Maybe String
 extractConversion [] = error "conv: No response, something weird is up."
-extractConversion ls = let
-    regex1 = mkRegex "<font size=\\+1><b>"
-    regex2 = mkRegex "</b>"
-    getConv a = do
-        (_,_,s,_) <- matchRegexAll regex1 a
-        (s',_,_,_) <- matchRegexAll regex2 s
-        return s'
+extractConversion ls = (getConv $ last ls) >>= return . pipeline replaceFuncs
+    where
+        regex1 = mkRegex "<font size=\\+1><b>"
+        regex2 = mkRegex "</b>"
 
-    searchAndReplace new regex = \s -> subRegex (mkRegex regex) s new
-    replaceFuncs = zipWith searchAndReplace
-                        [    "^",       "",      "x",                      ","]
-                        ["<sup>", "</sup>", "&#215;", "<font size=-2> </font>"]
-    pipeline [] a = a
-    pipeline (f:fs) a = pipeline fs $ f a
-    in
-    (getConv $ last ls) >>= return . pipeline replaceFuncs
+        getConv a = do
+            (_,_,s,_) <- matchRegexAll regex1 a
+            (s',_,_,_) <- matchRegexAll regex2 s
+            return s'
 
--- ---------------------------------------------------------------------
--- Testing only
+        searchAndReplace new regex = \s -> subRegex (mkRegex regex) s new
+        replaceFuncs = zipWith searchAndReplace
+                            [    "^",       "",      "x",                      ","]
+                            ["<sup>", "</sup>", "&#215;", "<font size=-2> </font>"]
 
-{-
-testHeaders :: [String]
-testHeaders =
-    ["HTTP/1.0 302 Found\r",
-     "Location: http://sf.net/projects/haskell-libs\r",
-     "Cache-Control: private\r",
-     "Set-Cookie: PREF=ID=702ec464c1b22b43:TM=1112828216:LM=1112828216"
-     ++ ":S=70y-S8HgcsMkVXWx; expires=Sun, 17-Jan-2038 19:14:07 GMT"
-     ++ "; path=/; domain=.google.com\r",
-     "Content-Type: text/html\r",
-     "Server: GWS/2.1\r",
-     "Content-Length: 165\r",
-     "Date: Wed, 06 Apr 2005 22:56:56 GMT\r",
-     "Connection: Keep-Alive\r",
-     "\r"]
--}
+        pipeline [] a = a
+        pipeline (f:fs) a = pipeline fs $ f a
