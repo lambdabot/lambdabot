39648d9254527faeb70c49c6679ef11ac366ca64
dons <dons@cse.unsw.edu.au>
2006-04-24 02:17:48 +0000

    comment only

--- scripts/Eval.hs
+++ scripts/Eval.hs
@@ -1,6 +1,9 @@
 --
 -- Helper code for runplugs
 --
+-- Note: must be kept in separate module to hide unsafePerformIO from
+-- runplugs-generated eval code!! you're warned.
+--
 module Eval where
 
 import Language.Haskell.TH
