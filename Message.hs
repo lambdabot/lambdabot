2a27b35cf3624953d8602eda8d2e3ef50f77aa9f
Dmitry Astapov <dastapov@gmail.com>
2006-05-30 10:14:04 +0000

    Added Message.hs - abstract interface to message contents This is a first part of abstracting away all transport details and adding XMPP transport. XMPP messages are XML elements and it would be cumbersome to transform them into the IRC prefix-command-params model. Hence the typeclass.

---
+++ Message.hs
@@ -0,0 +1,14 @@
+module Message where
+
+-- TODO: probably remove "Show a" later
+class Show a => Message a where
+  nick :: a -> String -- | extracts the nickname involved in a given message.
+  fullName :: a -> String -- | 'fullName' extracts the full user name involved in a given message.
+  names :: [String] -> a -- | 'names' builds a NAMES message from a list of channels.
+  channels :: a -> [String] -- | 'channels' extracts the channels a Message operate on.
+  joinChannel :: String -> a -- | 'join' creates a join message. String given is the location (channel) to join
+  partChannel :: String -> a -- | 'part' parts the channel given.
+  getTopic :: String -> a -- | 'getTopic' Returns the topic for a channel, given as a String
+  setTopic :: String -> String -> a -- | 'setTopic' takes a channel and a topic. It then returns the message
+                                    --   which sets the channels topic.
+  body :: a -> [String] -- TODO: recheck this. It's usage heavily relies on the fact that message comes from IRC
