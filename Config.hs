616502862e361213226eb9598ca5ee989df739e0
dons <dons@cse.unsw.edu.au>
2006-06-08 07:40:12 +0000

    perl6 is a channel too

--- Config.hs
+++ Config.hs
@@ -52,7 +52,7 @@ config = Config {
         proxy           = Just ("www-proxy",3128),
         autojoin        = ["#haskell","#haskell-blah","#flippi"
                           ,"#haskell-overflow","#gentoo-haskell"
-                          ,"#haskell_ru", "#darcs"
+                          ,"#haskell_ru", "#darcs" ,  "#perl6"
                           ,"#haskell.it","#haskell.se", "#haskell.es","#ScannedInAvian"],
 
 
