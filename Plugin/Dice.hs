daca762427d7414ca119832834c94509e8a0c51e
Bertram Felgenhauer <int-e@gmx.de>
2006-08-06 05:02:54 +0000

    Dice: more concise error messages, minor formula improvement for many dice.

--- Plugin/Dice.hs
+++ Plugin/Dice.hs
@@ -20,22 +20,24 @@ instance Module DiceModule () where
 
 dice :: String -> IO String
 dice str = case parse expr "dice" (filter (not.isSpace) str) of
-            Left err  -> return $ show err
+            Left err  -> return . trimError $ err
             Right e   -> do res <- eval e
                             return (brk 30 str++" => "++brk 45 (show res))
            where
             brk n s | length s <= n = s
                     | otherwise     = take (n-3) s ++ "..."
+            trimError = concat . intersperse ": " . tail . lines . show
 
 eval :: [(Integer, Integer)] -> IO Integer
 eval = foldM ef 0
     where ef acc (v,1) = return (acc+v)
           ef acc (n,d) = if n > 100
                             then do x <- ndRandomIO
-                                    let e = fromIntegral (n*(d+1))/2
+                                    let e = n*(d+1)`div`2
+                                        e' = fromIntegral (n*(d+1)`mod`2)/2
                                         v = fromIntegral (d*d-1)/12
-                                        x' = e + x * sqrt (fromIntegral n * v)
-                                    return (acc + round x')
+                                        x' = e' + x * sqrt (fromIntegral n * v)
+                                    return (acc + e + round x')
                             else do ls <- replicateM (fromIntegral n)
                                                      (randomRIO (1,d))
                                     return (acc + sum ls)
@@ -49,7 +51,7 @@ ndRandomIO = do r   <- randomRIO (0, 1)
 
 expr :: CharParser st [(Integer, Integer)]
 expr = do res <- primExp `sepBy1` (char '+')
-          eof
+          eof <?> "end"
           return res
 
 primExp :: CharParser st (Integer, Integer)
@@ -58,5 +60,5 @@ primExp = do v <- number
              return (v,d)
 
 number :: CharParser st Integer
-number = read `fmap` many1 digit
+number = read `fmap` many1 digit <?> "number"
 
