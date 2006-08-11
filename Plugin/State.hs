b0bcf20b6700b6aa9028f9eec9ad339cde16e0be
Don Stewart <dons@cse.unsw.edu.au>
2006-08-11 09:33:12 +0000

    add state cons/snoc

--- Plugin/State.hs
+++ Plugin/State.hs
@@ -9,9 +9,11 @@ import Plugin
 PLUGIN State
 
 instance Module StateModule String where
-    moduleCmds      _ = ["state"]
+    moduleCmds      _ = ["state","snoc","cons"]
     moduleHelp    _ _ = "state [expr]. Get or set a state variable."
     moduleDefState  _ = return "This page intentionally left blank."
     moduleSerialize _ = Just stdSerial
-    process_  _ _ []  = withMS $ \s _ -> return $ if null s then [] else [s]
-    process_ _ _ t    = withMS $ \_ w -> w t >> return [t]
+    process_  _ _ []      = withMS $ \s _ -> return $ if null s then [] else [s]
+    process_ _ "state"  t = withMS $ \_ w -> w t >> return [t]
+    process_ _ "snoc"  t = withMS $ \s w -> w t >> return [s++t]
+    process_ _ "cons"  t = withMS $ \s w -> w t >> return [t++s]
