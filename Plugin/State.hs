279ba0cc34e34a7202e3b3c99f7f4b59b0a32e16
Don Stewart <dons@cse.unsw.edu.au>
2006-08-11 09:45:28 +0000

    fix snoc/cons and add nil

--- Plugin/State.hs
+++ Plugin/State.hs
@@ -9,11 +9,12 @@ import Plugin
 PLUGIN State
 
 instance Module StateModule String where
-    moduleCmds      _ = ["state","snoc","cons"]
+    moduleCmds      _ = ["state","nil","snoc","cons"]
     moduleHelp    _ _ = "state [expr]. Get or set a state variable."
     moduleDefState  _ = return "This page intentionally left blank."
     moduleSerialize _ = Just stdSerial
     process_  _ _ []      = withMS $ \s _ -> return $ if null s then [] else [s]
-    process_ _ "state"  t = withMS $ \_ w -> w t >> return [t]
-    process_ _ "snoc"  t = withMS $ \s w -> w t >> return [s++t]
-    process_ _ "cons"  t = withMS $ \s w -> w t >> return [t++s]
+    process_ _ "state" t  = withMS $ \_ w -> w t >> return [t]
+    process_ _ "nil"   _  = withMS $ \_ w -> w [] >> return []
+    process_ _ "snoc"  t  = withMS $ \s w -> w (s++t) >> return [s++t]
+    process_ _ "cons"  t  = withMS $ \s w -> w (t++s) >> return [t++s]
