a3664082d99649c82df6bccd2fc72b1f99c8c688
Don Stewart <dons@cse.unsw.edu.au>
2006-08-11 10:11:39 +0000

    remember how to script the state

--- Plugin/State.hs
+++ Plugin/State.hs
@@ -16,3 +16,19 @@ instance Module StateModule String where
     process_  _ _ []      = withMS $ \s _ -> return $ if null s then [] else [s]
     process_ _ "state" t  = withMS $ \_ w -> w t >> return [t]
     process_ _ "++"    t  = withMS $ \s w -> w s >> return [s++t]
+
+{-
+
+An example of scripting lambdabot. Implementing Rock/paper/scissors:
+
+20:07  dons:: ?state (\xs -> case last (init xs) of '1' -> "rock"; '2' -> "paper"; '3' -> "scissors")
+20:07  lambdabot:: (\xs -> case last (init xs) of '1' -> "rock"; '2' -> "paper"; '3' -> "scissors")
+
+20:08  dons:: now, compose another plugin, with this state
+
+20:08  dons:: ?. read . run . ++ . show dice 1d3
+20:08  lambdabot::  paper
+20:08  dons:: ?. read . run . ++ . show dice 1d3
+20:08  lambdabot::  rock
+
+-}
