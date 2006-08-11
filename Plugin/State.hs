c9d6c346bafc78454ca6d604f576a015f9308304
Don Stewart <dons@cse.unsw.edu.au>
2006-08-11 10:03:04 +0000

    simplify

--- Plugin/State.hs
+++ Plugin/State.hs
@@ -9,12 +9,10 @@ import Plugin
 PLUGIN State
 
 instance Module StateModule String where
-    moduleCmds      _ = ["state","nil","snoc","cons"]
+    moduleCmds      _ = ["state","++"]
     moduleHelp    _ _ = "state [expr]. Get or set a state variable."
     moduleDefState  _ = return "This page intentionally left blank."
     moduleSerialize _ = Just stdSerial
     process_  _ _ []      = withMS $ \s _ -> return $ if null s then [] else [s]
     process_ _ "state" t  = withMS $ \_ w -> w t >> return [t]
-    process_ _ "nil"   _  = withMS $ \_ w -> w [] >> return []
-    process_ _ "snoc"  t  = withMS $ \s w -> w (s++t) >> return [s++t]
-    process_ _ "cons"  t  = withMS $ \s w -> w (t++s) >> return [t++s]
+    process_ _ "++"    t  = withMS $ \s w -> w s >> return [s++t]
