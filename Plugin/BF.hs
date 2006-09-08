2a469c9bbcbbad08b0c641d812a3e986b9aad10e
Jason Dagit <dagit@codersbase.com>
2006-09-08 06:47:35 +0000

    in BF.hs isAscii wasn't stringent enough so replaced with custom 'printable'

--- Plugin/BF.hs
+++ Plugin/BF.hs
@@ -41,5 +41,10 @@ bf src = do
 --
 cleanit :: String -> String
 cleanit s | Just _         <- terminated `matchRegex`    s = "Terminated\n"
-          | otherwise      = filter isAscii s
+          | otherwise      = filter printable s
     where terminated = mkRegex "waitForProc"
+          -- the printable ascii chars are in the range [33 .. 126]
+          -- according to wikipedia:
+          -- http://en.wikipedia.org/wiki/ASCII#ASCII_printable_characters
+          printable x = 32 < ord x && ord x < 127
+
