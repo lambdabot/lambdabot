2f9812d3050c0236c7fdaa3d5860c06b77dc4b20
dons <dons@cse.unsw.edu.au>
2006-11-07 00:11:20 +0000

    update

--- State/L.hs
+++ State/L.hs
@@ -39,16 +39,6 @@ import Test.QuickCheck
 import ShowQ
 
 {-# LINE 1 "<local>" #-}
-fac n = product [1..n]
-fib3' a b c d = if a `div` c == b `div` d then a `div` c else fib3' (a+b) a (c+d) c
-fib3 0 = 0
-fib3 1 = 1
-fib3 2 = 1
-fib3 3 = 2
-fib3 k = let { (q,r) = k `divMod` 2; a = fib3 q; b = fib3' (4*a+1) (2*a+1) 2 2 } in if r == 0 then 2*a*b - a*a else a*a + b*b
-test = 4
-snd3 (_,x,_) = x
-seqZip = ((tail >>=) . zipWith)
-sieve (p:ns) = p : sieve (filter (\n -> n `mod` p /= 0) ns)
-posRandom = let f x = (do (r, g') <- gets (randomR (0,3)); put g'; case r of 0 -> return x; 1 -> f (x * 2); _ -> f (x * 2 + 1)) in f 0 :: State StdGen Integer
-x = "x"
+splitBy p = unfoldr (\xs -> case (break p xs) of ([],_) -> Nothing; (xs,[]) -> Just (xs, []); (xs,ys) -> Just (xs, tail ys))
+on f g a b = (f a) `g` (f b)
+state = flip execState
