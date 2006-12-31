876e607635f10bbe50c868d59c522ee9decd4fc1
Don Stewart <dons@cse.unsw.edu.au>
2006-12-31 10:13:48 +0000

    implement regex matching for quotes

--- Plugin/Quote.hs
+++ Plugin/Quote.hs
@@ -3,17 +3,19 @@
 --
 module Plugin.Quote (theModule) where
 
-import Plugin.Quote.Fortune      (randFortune)
-import Plugin.Quote.Text
+import Lib.Regex as R
 
 import Plugin
+import Plugin.Quote.Fortune      (randFortune)
+import Plugin.Quote.Text
 
 import qualified Data.Map as M
 import qualified Data.ByteString.Char8 as P
 
 PLUGIN Quote
 
-type Quotes = M.Map P.ByteString [P.ByteString]
+type Key    = P.ByteString
+type Quotes = M.Map Key [P.ByteString]
 
 instance Module QuoteModule Quotes where
     moduleCmds           _ = ["quote", "remember", "ghc", "fortune"
@@ -40,25 +42,31 @@ instance Module QuoteModule Quotes where
     moduleDefState  _       = return M.empty
 
     process_ _ cmd s = case cmd of
-          "remember" -> runRemember (dropSpace s)
-          "quote"    -> runQuote    (dropSpace s)
-          "ghc"      -> runQuote    "ghc"
-          "fortune"  -> return `fmap` io (randFortune Nothing)
-          "yow"      -> return `fmap` io (randFortune (Just "zippy"))
-          "keal"     -> return `fmap` io (randomElem kealList)
-	  -- See, you've got to understand the subtle distinction in pirate
-	  -- talk between arr and yarr! arr is something you say as an
-	  -- afermative where as yarr! is more like a greeting. (Or something)
-          "arr"      -> return `fmap` io (randomElem arrList)
-          "yarr"     -> return `fmap` io (randomElem yarrList)
-          "b52s"     -> return `fmap` io (randomElem b52s)
-          "brain"    -> return `fmap` io (randomElem brain)
-          "palomer"  -> return `fmap` io (randomElem palomer)
-          "girl19"   -> return `fmap` io (randomElem girl19)
-          "protontorpedo" -> return `fmap` io (randomElem protontorpedo)
-          "v"        -> return `fmap` io (randomElem notoriousV)
+          "remember"      -> runRemember (dropSpace s)
+          "quote"         -> runQuote    (dropSpace s)
+          "ghc"           -> runQuote    ("ghc " ++ dropSpace s)
+          "fortune"       -> run (randFortune Nothing)
+          "yow"           -> run (randFortune (Just "zippy"))
+
+          "keal"          -> random kealList
+          "b52s"          -> random b52s
+          "brain"         -> random brain
+          "palomer"       -> random palomer
+          "girl19"        -> random girl19
+          "protontorpedo" -> random protontorpedo
+          "v"             -> random notoriousV
           "yhjulwwiefzojcbxybbruweejw"
-                     -> return `fmap` io (randomElem notoriousV)
+                          -> random notoriousV
+
+          -- See, you've got to understand the subtle distinction in pirate
+          -- talk between arr and yarr! arr is something you say as an
+          -- afermative where as yarr! is more like a greeting. (Or something)
+          "arr"           -> random arrList
+          "yarr"          -> random yarrList
+
+        where
+           run k = return `fmap` io k
+           random = run . randomElem
 
 help :: String
 help = "quote <nick>\nremember <nick> <quote>\n" ++
@@ -71,36 +79,94 @@ help = "quote <nick>\nremember <nick> <quote>\n" ++
 
 -- error handling!
 runRemember :: String -> ModuleLB Quotes
-runRemember str = do
-    case break (== ' ') str of
-        (_,[])    -> return ["Incorrect arguments to quote"]
-        (nm,q') -> do let q = tail q'
-                      withMS $ \fm writer -> do
-                        let ss  = fromMaybe [] (M.lookup (P.pack nm) fm)
-                            fm' = M.insert (P.pack nm) (P.pack q : ss) fm
-                        writer fm'
-                        return []
+runRemember str
+    | null rest = return ["Incorrect arguments to quote"]
+    | otherwise = withMS $ \fm writer -> do
+        let ss  = fromMaybe [] (M.lookup (P.pack nm) fm)
+            fm' = M.insert (P.pack nm) (P.pack q : ss) fm
+        writer fm'
+        return []
+    where
+        (nm,rest) = break isSpace str
+        q         = tail rest
 
 --
 --  the @quote command, takes a user nm to choose a random quote from
 --
 runQuote :: String -> ModuleLB Quotes
-runQuote name' = do
-    fm <- readMS
-    if M.null fm then return ["No quotes yet."] else do
+runQuote str = do
+    st <- readMS
+    io (search (P.pack nm) (P.pack pat) st)
+  where (nm, p) = break isSpace str
+        pat     = if null p then p else tail p
 
-        let pnm = P.pack name'
-            qs' = M.lookup pnm fm
+search :: Key -> P.ByteString -> Quotes -> IO [String]
+search key pat db
+    | M.null db          = box "No quotes yet."
 
-        (nm,qs) <- if not (P.null pnm)
-                   then return (pnm,qs') -- (ByteString, Maybe [ByteString])
-                   else do (nm',rs') <- io $ randomElem (M.toList fm) -- random person
-                           return (nm', Just rs')
-        case qs of
-            Nothing   -> return [P.unpack nm ++ " hasn't said anything memorable"]
-            Just msgs -> do msg <- io $ randomElem msgs
-                            return $ if not (P.null pnm)
-                                then ["  " ++ (P.unpack msg)]
-                                else [(P.unpack nm)++" says: " ++ (P.unpack msg)]
+    | P.null key         = do
+        (key', qs) <- random (M.toList db) -- quote a random person
+        box . display key' =<< random qs
 
+    | P.null pat, Just qs <- mquotes =
+        box . display key  =<< random qs
 
+    | P.null pat         = match key allquotes
+
+    | Just qs <- mquotes = match pat qs
+
+    | otherwise          = do
+        r <- random insult
+        box $ "No quotes for this person. " ++ r
+
+  where
+    box       = return . (:[])
+    mquotes   = M.lookup key db
+    allquotes = concat (M.elems  db)
+    random    = randomElem
+
+    match p ss = do
+        re <- R.regcomp p $ regExtended + regIgnoreCase
+        let rs = filter (R.matchRegex' re) ss
+        if null rs
+            then do r <- random insult
+                    box $ "No quotes match. " ++ r
+            else box . P.unpack =<< random rs
+
+    display k msg = (if P.null k then "  " else who ++ " says: ") ++ saying
+          where saying = P.unpack msg
+                who    = P.unpack k
+
+--
+-- Amusing insults from OpenBSD sudo
+--
+insult :: [String]
+insult =
+   ["Just what do you think you're doing Dave?",
+    "It can only be attributed to human error.",
+    "That's something I cannot allow to happen.",
+    "My mind is going. I can feel it.",
+    "Sorry about this, I know it's a bit silly.",
+    "Take a stress pill and think things over.",
+    "This mission is too important for me to allow you to jeopardize it.",
+    "I feel much better now.",
+
+    "Wrong!  You cheating scum!",
+    "And you call yourself a Rocket Scientist!",
+    "Where did you learn to type?",
+    "Are you on drugs?",
+    "My pet ferret can type better than you!",
+    "You type like i drive.",
+    "Do you think like you type?",
+    "Your mind just hasn't been the same since the electro-shock, has it?",
+
+    "Maybe if you used more than just two fingers...",
+    "BOB says:  You seem to have forgotten your passwd, enter another!",
+    "stty: unknown mode: doofus",
+    "I can't hear you -- I'm using the scrambler.",
+    "The more you drive -- the dumber you get.",
+    "Listen, broccoli brains, I don't have time to listen to this trash.",
+    "I've seen penguins that can type better than that.",
+    "Have you considered trying to match wits with a rutabaga?",
+    "You speak an infinite deal of nothing"
+    ]
