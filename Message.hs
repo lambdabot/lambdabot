428d8e2e43886cdf90cdd7b9fd11c8ce5fd6a935
stefanor <stefanor@cox.net>
2007-01-01 21:05:28 +0000

    add types for attaching server names to messages

--- Message.hs
+++ Message.hs
@@ -7,35 +7,69 @@ import Control.Concurrent
 
 -- TODO: probably remove "Show a" later
 class Show a => Message a where
+    -- | extracts the tag of the server involved in a given message
+    server      :: a -> String
+
     -- | extracts the nickname involved in a given message.
-    nick        :: a -> String
+    nick        :: a -> Nick
 
     -- | 'fullName' extracts the full user name involved in a given message.
     fullName    :: a -> String
 
     -- | 'names' builds a NAMES message from a list of channels.
-    names       :: [String] -> a
+    names       :: [Nick] -> a
 
     -- | 'channels' extracts the channels a Message operate on.
-    channels    :: a -> [String]
+    channels    :: a -> [Nick]
 
     -- | 'join' creates a join message. String given is the location (channel) to join
-    joinChannel :: String -> a
+    joinChannel :: Nick -> a
 
     -- | 'part' parts the channel given.
-    partChannel :: String -> a
+    partChannel :: Nick -> a
 
     -- | 'getTopic' Returns the topic for a channel, given as a String
-    getTopic    :: String -> a
+    getTopic    :: Nick -> a
 
     -- | 'setTopic' takes a channel and a topic. It then returns the message
     --   which sets the channels topic.
-    setTopic :: String -> String -> a
+    setTopic :: Nick -> String -> a
 
     -- TODO: recheck this. It's usage heavily relies on the fact that message comes from IRC
     body :: a -> [String]
 
     -- TODO: too IRC-specific
     command :: a -> String
+
+    -- TODO: there must be a better way of handling this ...
+    lambdabotName :: a -> Nick
+
+-- |The type of nicknames isolated from a message.
+data Nick
+  = Nick {
+        nTag :: !String, -- ^The tag of the server this nick is on
+        nName :: !String -- ^The server-specific nickname of this nick
+  } deriving (Eq,Ord)
+
+-- |Format a nickname for display.  This will automatically omit the server
+-- field if it is the same as the server of the provided message.
+showNick :: Message a => a -> Nick -> String
+showNick msg nick | nTag nick == server msg = nName nick
+                  | otherwise               = nTag nick ++ ':' : nName nick
+
+-- |Parse a nickname received in a message.  If the server field is not
+-- provided, it defaults to the same as that of the message.
+readNick :: Message a => a -> String -> Nick
+readNick msg str | null ac   = Nick (server msg) str
+                 | otherwise = Nick bc (tail ac)
+    where (bc, ac) = break (==':') str
+
+instance Show Nick where
+    show nick = show (nTag nick ++ ':' : nName nick)
+
+instance Read Nick where
+    read str | null ac   = Nick "fn" bc
+             | otherwise = Nick bc (tail ac)
+        where (bc, ac) = break (==':') (read str)
 
 type Pipe a = Chan (Maybe a)
