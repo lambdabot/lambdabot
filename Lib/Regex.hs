d8c1eefb8419e7971c2b1e28105939df68e46007
Don Stewart <dons@cse.unsw.edu.au>
2007-01-01 05:13:08 +0000

    And back port new regex interface to 6.4

--- Lib/Regex.hs
+++ Lib/Regex.hs
@@ -1,3 +1,13 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Lib.Regex
+-- Copyright   :  (c) Don Stewart 2007
+-- License     :  GPL (see LICENSE)
+-- 
+-- Maintainer  :  dons@cse.unsw.edu.au
+--
+-----------------------------------------------------------------------------
+
 module Lib.Regex (
         -- ByteString interface
         regex,      -- :: ByteString -> Regex 
@@ -12,17 +22,36 @@ module Lib.Regex (
 
     ) where
 
-import Text.Regex.Posix.ByteString
 import System.IO.Unsafe (unsafePerformIO)
 import Data.ByteString.Char8
 
+#if __GLASGOW_HASKELL__ >= 606
+import Text.Regex.Posix.ByteString
+#else
+import Text.Regex
+#endif
+
+------------------------------------------------------------------------
 --
 -- convenient regex wrappers:
 --
-regex' :: String -> Regex
+
+regex'   ::     String -> Regex
+regex    :: ByteString -> Regex
+
+matches' :: Regex ->     String -> Bool
+matches  :: Regex -> ByteString -> Bool
+
+------------------------------------------------------------------------
+
+#if __GLASGOW_HASKELL__ >= 606
+
+--
+-- For ghc 6.6 we use the regex-posix bytestring library
+--
+
 regex' s = regex (pack s)
 
-regex :: ByteString -> Regex
 regex p = unsafePerformIO $ do
     res <- compile compileFlags execFlags p
     case res of
@@ -35,10 +64,8 @@ regex p = unsafePerformIO $ do
 --
 -- match a regex against a string or bytestring
 --
-matches' :: Regex -> String -> Bool
 matches' r s = matches r (pack s)
 
-matches  :: Regex -> ByteString -> Bool
 matches r p = unsafePerformIO $ do
     res <- execute r p
     case res of
@@ -46,3 +73,18 @@ matches r p = unsafePerformIO $ do
         Right Nothing  -> return False
         Right (Just _) -> return True
 
+#else
+
+--
+-- ghc 6.4.x Text.Regex compat:
+--
+
+regex'  = mkRegex
+regex   = regex' . unpack
+
+matches' r s | Just _ <- matchRegex r s = True
+             | otherwise                = False
+
+matches  r s = matches' r (unpack s)
+
+#endif
