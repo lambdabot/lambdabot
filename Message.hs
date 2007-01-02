504003842b191478817ce617339e8c2bd4746466
stefanor <stefanor@cox.net>
2007-01-02 00:02:03 +0000

    refactor Message, add (un)packing

--- Message.hs
+++ Message.hs
@@ -1,9 +1,12 @@
 --
 -- Provides interface to messages, message pipes
 --
-module Message(Message(..), Nick(..), showNick, readNick, Pipe) where
+module Message(Message(..), Nick(..), showNick, readNick, Pipe, packNick, unpackNick) where
 
+import qualified Data.ByteString.Char8 as P
 import Control.Concurrent
+
+import Control.Arrow( first )
 
 -- TODO: probably remove "Show a" later
 class Show a => Message a where
@@ -51,26 +54,39 @@ data Nick
         nName :: !String -- ^The server-specific nickname of this nick
   } deriving (Eq,Ord)
 
+-- Helper functions
+upckStr :: String -> String -> Nick
+upckStr def str | null ac   = Nick def str
+                | otherwise = Nick bc (tail ac)
+    where (bc, ac) = break (==':') str
+
+pckStr :: Nick -> String
+pckStr nck = nTag nck ++ ':' : nName nck
+
 -- |Format a nickname for display.  This will automatically omit the server
 -- field if it is the same as the server of the provided message.
 showNick :: Message a => a -> Nick -> String
 showNick msg nick_ | nTag nick_ == server msg = nName nick_
-                   | otherwise                = nTag nick_ ++ ':' : nName nick_
+                   | otherwise                = pckStr nick_
 
 -- |Parse a nickname received in a message.  If the server field is not
 -- provided, it defaults to the same as that of the message.
 readNick :: Message a => a -> String -> Nick
-readNick msg str | null ac   = Nick (server msg) str
-                 | otherwise = Nick bc (tail ac)
-    where (bc, ac) = break (==':') str
+readNick msg str = upckStr (server msg) str
 
 instance Show Nick where
-    show nick_ = show (nTag nick_ ++ ':' : nName nick_)
+    show = show . pckStr
 
 instance Read Nick where
-    readsPrec prec str = map parse' (readsPrec prec str)
-        where parse' (str',aft) | null ac   = (Nick "fn" bc, aft)
-                                | otherwise = (Nick bc (tail ac), aft)
-                  where (bc, ac) = break (==':') str'
+    readsPrec prec str = map (first (upckStr "fn")) (readsPrec prec str)
+
+-- |Pack a nickname into a ByteString.  Note that the resulting strings are
+-- not optimally formatted for human consumtion.
+packNick :: Nick -> P.ByteString
+packNick = P.pack . pckStr
+
+-- |Unpack a nickname packed by 'packNick'.
+unpackNick :: P.ByteString -> Nick
+unpackNick = upckStr "fn" . P.unpack
 
 type Pipe a = Chan (Maybe a)
