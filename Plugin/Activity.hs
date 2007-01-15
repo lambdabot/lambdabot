a0cc1a80b51dd290c28fcc715ff3bd9252f0076d
Stefan O'Rear <stefanor@cox.net>
2007-01-15 10:39:56 +0000

    create a admin-only @activity-full ; obscure usernames in normal @activity

--- Plugin/Activity.hs
+++ Plugin/Activity.hs
@@ -19,17 +19,20 @@ type ActivityState = [(ClockTime,Msg.Nick)]
 instance Module ActivityModule ActivityState where
     moduleHelp _ _              = "activity seconds. Find out where/how much the bot is being used"
     moduleCmds   _              = ["activity"]
+    modulePrivs  _              = ["activity-full"]
     moduleDefState _            = return []
     moduleInit   _              = bindModule2 activityFilter >>=
                                       ircInstallOutputFilter
-    process      _ msg _ _ rest = do
+    process      _ msg _ cmd rest = do
         TOD secs ps <- io getClockTime
         let cutoff = TOD (secs - (fromMaybe 90 $ readM rest)) ps
-        users <- (map snd . takeWhile ((> cutoff) . fst)) `fmap` readMS
+        users <- (map (obscure . snd) . takeWhile ((> cutoff) . fst)) `fmap` readMS
         let agg_users = reverse . sort . map (length &&& head) . group . sort $ users
         let fmt_agg = concatWith " " . (:) (show (length users) ++ "*total") .
                       map (\(n,u) -> show n ++ "*" ++ Msg.showNick msg u) $ agg_users
         return [fmt_agg]
+      where obscure nm | cmd == "activity-full" || isPrefixOf "#" (Msg.nName nm) = nm
+                       | otherwise = Msg.readNick msg "private"
 
 activityFilter :: Msg.Nick -> [String] -> ModuleLB ActivityState
 activityFilter target lns = do io $ evaluate $ foldr seq () $ map (foldr seq ()) $ lns
