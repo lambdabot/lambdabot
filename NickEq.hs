c8b292f36b32afb5c846a39898e9430c7e3c54c3
Stefan O'Rear <stefanor@cox.net>
2007-01-22 04:01:26 +0000

    nick identity, act 1 - the base API

---
+++ NickEq.hs
@@ -0,0 +1,36 @@
+--
+-- | Nickname equality subsystem.
+--
+-- This component is responsible for deciding whether two nicknames
+-- refer to the same person, for the purposes of @tell et al.  Nickname
+-- equality must be monadic because it uses mutable state maintained
+-- by the @link and @unlink commands.
+--
+-- Also provided is a concept of polynicks (by analogy to polytypes);
+-- polynicks can refer to an (open) set of nicknames.  For instance '@tell
+-- *lambdabot Why does X do Y' could tell a message to anyone who has
+-- identified as a lambdabot maintainer.  A polynick consists of a
+-- bar-separated list of (nicks or open terms); an open term is like a
+-- nick but preceded with a star.
+
+module NickEq ( Polynick, nickMatches, readPolynick, showPolynick ) where
+
+import Message( Nick, readNick, showNick )
+import Lambdabot
+
+data Polynick = Polynick Nick -- for now
+
+-- |Determine if a nick matches a polynick.  The state is read at the
+-- point of binding.
+nickMatches :: LB (Nick -> Polynick -> Bool)
+nickMatches = return m'
+    where
+      m' nck (Polynick nck2) = nck == nck2
+
+-- | Parse a read polynick.
+readPolynick :: Message a => a -> String -> Polynick
+readPolynick m = Polynick . readNick
+
+-- | Format a polynick.
+showPolynick :: Message a => a -> Polynick -> String
+showPolynick m (Polynick n) = showNick m n
