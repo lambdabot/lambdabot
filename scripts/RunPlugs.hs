16cfb2da0049abf9a07c1d287863db517cb2d6ea
Don Stewart <dons@cse.unsw.edu.au>
2007-02-14 00:24:25 +0000

    -fasm

--- scripts/RunPlugs.hs
+++ scripts/RunPlugs.hs
@@ -122,7 +122,7 @@ main = do
         s <- unsafeEval_ ("let { "++x++
                          " = \n# 1 \"<irc>\"\n"++s++
                          "\n} in P.take 2048 (P.show "++x++
-                         ")") context ["-O","-fextended-default-rules"] [] []
+                         ")") context ["-O","-fasm","-fextended-default-rules"] [] []
         case s of
             Left  e -> mapM_ putStrLn e
             Right v -> Control.Exception.catch
