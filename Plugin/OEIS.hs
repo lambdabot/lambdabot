4ac469cfe6f73382a82c459e642583d7a1e3e11b
James Cook <mokus@deepbondi.net>
2008-02-17 15:31:15 +0000

    @sequence as new alias to @oeis

--- Plugin/OEIS.hs
+++ Plugin/OEIS.hs
@@ -10,7 +10,7 @@ import Math.OEIS
 $(plugin "OEIS")
 
 instance Module OEISModule () where
-    moduleCmds   _     = ["oeis"]
+    moduleCmds   _     = ["oeis", "sequence"]
     moduleHelp _ _     = "oeis <sequence>. Look up a sequence in the Online Encyclopedia of Integer Sequences"
     process _ _ to _ a = do s <- liftIO $ lookupOEIS a
                             out <- mapM (ios80 to) (map return s)
