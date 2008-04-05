8a7a94aedb4f5d21baf3779441ff42c94deb6160
cgibbard <cgibbard@gmail.com>
2008-04-05 02:43:13 +0000

    add imports.h !!

---
+++ imports.h
@@ -0,0 +1,71 @@
+import qualified Prelude as P
+import Prelude hiding (mapM, sequence, mapM_, sequence_, (.), (++), map)
+import Numeric
+import System.Random
+import Data.Array
+import Data.Bits
+import Data.Bool
+import Data.Char
+import Data.Complex
+import Data.Dynamic
+import Data.Either
+import Data.Eq
+import Data.Fixed
+import qualified Data.Foldable
+import Data.Function hiding ((.))
+import Data.Ord
+import qualified Data.Generics
+import Data.Graph
+import Data.Int
+import Data.Ix
+import Data.List hiding ((++),map)
+import Data.Maybe
+import Data.Monoid
+import Data.Ratio
+import qualified Data.Sequence
+import Data.Tree
+import Data.Tuple
+import qualified Data.Traversable
+import Data.Typeable
+import Data.Word
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.IntSet as IS
+import qualified Data.IntMap as IM
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Lazy.Char8 as BSLC
+import Data.Number.Symbolic
+import Data.Number.Dif
+import Data.Number.CReal
+import Data.Number.Fixed
+import Data.Number.Interval
+import Data.Number.BigFloat
+import Data.Number.Natural
+import Control.Monad
+import Control.Monad.Cont
+import Control.Monad.Identity
+import Control.Monad.State
+import Control.Monad.State
+import Control.Monad.ST
+import Control.Monad.Reader
+import Control.Monad.Fix
+import Control.Monad.Writer
+import Control.Monad.RWS
+import Control.Monad.Error
+import Control.Monad.Instances
+import Control.Arrow hiding (pure)
+import qualified Control.Arrow.Transformer as AT
+import qualified Control.Arrow.Transformer.All as AT
+import Control.Arrow.Operations
+import Control.Applicative
+import Control.Parallel
+import Control.Parallel.Strategies
+import Text.Printf
+import Text.PrettyPrint.HughesPJ hiding (empty)
+import Test.QuickCheck
+import ShowQ
+import LargeWord
+import SimpleReflect hiding (var)
+import Math.OEIS
