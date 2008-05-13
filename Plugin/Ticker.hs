c9385141d8b738adfff3d41a485e25ddeffadcfb
newsham <newsham@lava.net>
2008-05-13 18:32:43 +0000

    simplified ticker and removed broken feature.

--- Plugin/Ticker.hs
+++ Plugin/Ticker.hs
@@ -24,8 +24,9 @@ quoteCmd _ ticker = do
       Just r -> return $ [r]
       Nothing  -> return ["No Result Found."]
 
+-- fetch: s symbol, l1 price, c change with percent, d1 date, t1 time.
 quoteUrl :: String -> String
-quoteUrl ticker =  "http://download.finance.yahoo.com/d/quotes.csv?f=nsl1d1t1c1b2&e=.csv&s=" ++ urlEncode ticker
+quoteUrl ticker =  "http://download.finance.yahoo.com/d/quotes.csv?f=sl1cd1t1&e=.csv&s=" ++ urlEncode ticker
 
 queryit :: String -> String -> IO [String]
 queryit meth url = readPage (proxy config) uri request ""
@@ -43,6 +44,7 @@ splitWhile p xs = (takeWhile p xs, dropWhile p xs)
 csv :: String -> [String]
 csv ('"':xs) = case splitWhile (/= '"') xs of
                   (word, '"':',':rest) -> word : csv rest
+                  (word, '"':[])       -> word : []
                   _                    -> error "invalid CSV"
 csv xs = case splitWhile (/= ',') xs of
              (word, ',':rest) -> word : csv rest
@@ -50,13 +52,14 @@ csv xs = case splitWhile (/= ',') xs of
              (word, [])       -> [word]
              _                -> error "shouldn't happen"
 
--- example output:
--- "S&P DEP RECEIPTS","SPY",138.32,"4/24/2008","4:15pm",+0.60,138.32
+-- $ curl "http://download.finance.yahoo.com/d/quotes.csv?f=sl1cd1t1&e=.csv&s=C"
+-- "C",23.19,"-0.45 - -1.90%","5/13/2008","1:32pm"
+-- "GBPUSD=X",1.9478,"N/A - N/A","5/13/2008","1:52pm"
 extractQuote :: [String] -> Maybe String
 extractQuote [] = Nothing
-extractQuote ls = (getQuote . csv . last) ls
+extractQuote ls = (getQuote . csv . filter (/= '\r') . last) ls
     where
-        getQuote (_:ticker:price:date:time:change:afterhrs:_) =
+        getQuote (_:ticker:price:date:time:change:afterhrs:_) = 
             Just $ printf "%s: %s %s %s @ %s %s %s" ticker' price change perc date' time' afterhrs'
             where ticker' = unquote ticker
                   time' = unquote time
@@ -70,9 +73,4 @@ extractQuote ls = (getQuote . csv . last) ls
                             then "-"
                             else printf "(%.1f%%)" (100.0 * change' / (price' - change'))
         getQuote _ = Nothing
-        unquote cs = filter (/= '"') cs
-        val :: String -> Double
-	val "N/A" = 0.0
-        val ('+':xs) = read xs
-        val xs = read xs
 
