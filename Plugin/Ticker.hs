3b14929872652ee13af5b848a9f8ad8edf8ca619
newsham <newsham@lava.net>
2008-06-29 03:29:08 +0000

    rewrite condor command as more general bid command

--- Plugin/Ticker.hs
+++ Plugin/Ticker.hs
@@ -3,39 +3,43 @@
 -- | Pull quotes down from yahoo.
 module Plugin.Ticker (theModule) where
 
-import Plugin
-import Control.Arrow
+import Plugin hiding((<$>))
+import Data.Either
+import Control.Applicative ((<$>))
+-- import Control.Arrow (second)
 import Text.Printf
 
 PLUGIN Ticker
 
 instance Module TickerModule () where
-    moduleHelp _ s      = case s of
-         "ticker"     -> "quote ticker.  Look up quotes for ticker"
-    moduleCmds      _   = ["ticker", "condor"]
+    moduleCmds      _     = ["ticker", "bid"]
+    moduleHelp _ s        = case s of
+         "ticker" -> "ticker symbols.  Look up quotes for symbols"
+         "bid"    -> "bid symbols.  Sum up the bid and ask prices for symbols."
     process_ _ "ticker" e = lift $ tickerCmd e
-    process_ _ "condor" e = lift $ condorCmd e
+    process_ _ "bid"    e = lift $ bidsCmd e
 
 ------------------------------------------------------------------------
 
+-- Fetch several ticker quotes and report them.
 tickerCmd :: String -> LB [String]
 tickerCmd []        = return ["Empty ticker."]
 tickerCmd tickers = do
-    body    <- io $ fetchPage "GET" (tickerUrl tickers)
-    case [x | Just x <- map extractQuote body] of
+    quotes <- io $ fetchPage "GET" $ tickerUrl $ words tickers
+    case [x | Just x <- map extractQuote quotes] of
       []       -> return ["No Result Found."]
       xs       -> return xs
 
 -- fetch: s symbol, l1 price, c change with percent, d1 date, t1 time.
-tickerUrl :: String -> String
+tickerUrl :: [String] -> String
 tickerUrl tickers =  "http://download.finance.yahoo.com/d/quotes.csv?f=sl1cd1t1&e=.csv&s=" ++ ts
-    where ts = concatWith "+" $ map urlEncode $ words tickers
+    where ts = concatWith "+" $ map urlEncode tickers
 
 -- $ curl "http://download.finance.yahoo.com/d/quotes.csv?f=sl1cd1t1&e=.csv&s=C"
 -- "C",23.19,"-0.45 - -1.90%","5/13/2008","1:32pm"
 -- "GBPUSD=X",1.9478,"N/A - N/A","5/13/2008","1:52pm"
 extractQuote :: String -> Maybe String
-extractQuote s = (getQuote . csv) s
+extractQuote = getQuote . csv
     where
         getQuote [sym, price, change, date, time] =
             Just $ printf "%s: %s %s@ %s %s" sym price change' date time
@@ -45,45 +49,58 @@ extractQuote s = (getQuote . csv) s
                               _            -> ""
         getQuote _ = Nothing
 
-condorCmd :: String -> LB [String]
-condorCmd [] = return ["Empty ticker list"]
-condorCmd tickers = 
-    case splitList '/' tickers of
-        [a,b,c,d] -> liftM (:[]) $ io $ calcCondor a b c d
-        _            -> return [printf "Invalid argument '%s'" tickers]
+-- Fetch quotes for tickers and sum their bid/ask prices.
+bidsCmd :: String -> LB [String]
+bidsCmd tickers = 
+    case words tickers of
+        [] -> return [printf "Invalid argument '%s'" tickers]
+        xs -> io $ (:[]) <$> calcBids xs
 
 -- fetch: b bid, a ask
-condorUrl :: [String] -> String
-condorUrl tickers = "http://download.finance.yahoo.com/d/quotes.csv?f=ba&e=.csv&s=" ++ ts
+bidsUrl :: [String] -> String
+bidsUrl tickers = "http://download.finance.yahoo.com/d/quotes.csv?f=ba&e=.csv&s=" ++ ts
     where ts = concatWith "+" $ map urlEncode tickers
 
 getBidAsks :: [String] -> IO [Maybe (Float, Float)]
 getBidAsks tickers = do
-    xs <- fetchPage "GET" (condorUrl tickers)
+    xs <- fetchPage "GET" $ bidsUrl tickers
     return $ map (extractPrice.csv) xs
     where
         extractPrice :: [String] -> Maybe (Float, Float)
         extractPrice [bid,ask] = liftM2 (,) (readMaybe bid) (readMaybe ask)
         extractPrice _         = Nothing
 
-calcCondor :: String -> String -> String -> String -> IO String
-calcCondor a b c d = do
-    xs <- getBidAsks [a,b,c,d]
-    return $ case xs of
-        [Just (ab,aa), Just (bb,ba), Just (cb,ca), Just (db,da)] -> 
-              printf "%s: bid $%.02f  ask $%.02f" s (bb+cb-aa-da) (ba+ca-ab-db)
-        [Nothing, _, _, _] -> printf "Can't find '%s'" a
-        [_, Nothing, _, _] -> printf "Can't find '%s'" b
-        [_, _, Nothing, _] -> printf "Can't find '%s'" c
-        [_, _, _, Nothing] -> printf "Can't find '%s'" d
-        _                  -> "calcCondor: Internal error"
-    where s = a ++ "/" ++ b ++ "/" ++ c ++ "/" ++ d
+type AccumVal = Either String (Float, Float)
+
+-- If we have a new bid/ask pair, accumulate it (normally add, but
+-- if the ticker starts with '-' then subtract).  If there is no
+-- value, make a note that it is an error.
+accumOption :: AccumVal -> (String, Maybe (Float, Float)) -> AccumVal
+accumOption err@(Left _) _ = err
+accumOption (Right _) (ticker, Nothing) = Left $ printf "Can't find '%s'" ticker
+accumOption (Right (a,b)) (('-':_), Just (a',b')) = Right (a-b', b-a')
+accumOption (Right (a,b)) (_, Just (a',b')) = Right (a+a', b+b')
+
+-- Take a list of tickers which are optionally prefixed with '+' or '-'
+-- and add up (or subtract) the bid/ask prices on the based on the prefix.
+calcBids :: [String] -> IO String
+calcBids ticks = do
+    xs <- getBidAsks $ map noPrefix ticks
+    return $ case foldl accumOption (Right (0,0)) (zip ticks xs) of
+        (Left err)        -> err
+        (Right (bid,ask)) -> printf "%s: bid $%.02f, ask $%.02f" s bid ask
+    where 
+        s = unwords ticks
+        noPrefix ('+':xs) = xs
+        noPrefix ('-':xs) = xs
+        noPrefix xs = xs
+        
 
 ---- Library routines, consider moving elsewhere. ----
 
 -- | Fetch a page via HTTP and return its body as a list of lines.
 fetchPage :: String -> String -> IO [String]
-fetchPage meth url = liftM cleanup $ readPage (proxy config) uri request ""
+fetchPage meth url = cleanup <$> readPage (proxy config) uri request ""
     where Just uri = parseURI url
           abs_path = uriPath uri ++ uriQuery uri ++ uriFragment uri
           request  = case proxy config of
@@ -97,13 +114,17 @@ splitWhile :: (a -> Bool) -> [a] -> ([a], [a])
 splitWhile p xs = (takeWhile p xs, dropWhile p xs)
 
 -- | Unfoldr, continuing while the predicate is true.
+{-
 unfoldrp :: (b -> Bool) -> (b -> Maybe (a, b)) -> b -> [a]
 unfoldrp p f = unfoldr (\x -> guard (p x) >> f x)
+-}
 
 -- | Split a list on a distinguished character (which is eliminated).
+{-
 splitList :: (Eq a) => a -> [a] -> [[a]]
 splitList ch = unfoldrp (not.null) (return.f)
     where f = (second (drop 1)) . (splitWhile (/= ch))
+-}
 
 -- | Return a list of comma-separated values.
 -- Quotes allowed in CSV if it's the first character of a field.
