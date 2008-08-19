1902bcad13f42f7ea58425d9c0c2a1969169d1c9
gwern0 <gwern0@gmail.com>
2008-08-19 23:49:05 +0000

    rm redefinitions of basic functions from State/Pristine.hs & L.hs There are any number of reasons to remove these redefinitions: * they confuse newbies in #haskell, since their code may no longer work in Lambdabot, and vice versa * they confuse even experienced haskellers * #haskell is not the place for testing out extensions or subtle far-reaching modifications to Haskell. * They make things more difficult for tools like mueval, and so on. Multiple haskellers have advocated for a long time that these be removed, so I'm doing it.

--- State/L.hs
+++ State/L.hs
@@ -75,24 +75,7 @@ import qualified Prelude as P
 import SimpleReflect hiding (var)
 import Math.OEIS
 
-
 describeSequence = fmap description . lookupSequence
-
-infixr 9 .
-
-(.) :: Functor f => (a -> b) -> f a -> f b
-(.) = fmap
-
-map :: Functor f => (a -> b) -> f a -> f b
-map = fmap
-
-infixr 5 ++
-
-(++) :: Monoid m => m -> m -> m
-(++) = mappend
-
-zero :: Monoid m => m
-zero = mempty
 
 newtype Mu f = In { out :: f (Mu f) }
 
--- State/Pristine.hs
+++ State/Pristine.hs
@@ -77,22 +77,6 @@ import Math.OEIS
 
 describeSequence = fmap description . lookupSequence
 
-infixr 9 .
-
-(.) :: Functor f => (a -> b) -> f a -> f b
-(.) = fmap
-
-map :: Functor f => (a -> b) -> f a -> f b
-map = fmap
-
-infixr 5 ++
-
-(++) :: Monoid m => m -> m -> m
-(++) = mappend
-
-zero :: Monoid m => m
-zero = mempty
-
 newtype Mu f = In { out :: f (Mu f) }
 
 newtype Rec a = InR { outR :: Rec a -> a }
