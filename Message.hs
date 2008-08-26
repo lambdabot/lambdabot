b846cf70005145ce80fc466a4265c77a604ea3d4
gwern0 <gwern0@gmail.com>
2008-08-26 19:51:34 +0000

    fmt Message.hs

--- Message.hs
+++ Message.hs
@@ -4,10 +4,10 @@
 module Message(Message(..), Nick(..), showNick, readNick, Pipe, packNick, unpackNick) where
 
 import qualified Data.ByteString.Char8 as P
-import Control.Concurrent
-import Data.Char(toUpper)
+import Control.Concurrent (Chan)
+import Data.Char (toUpper)
 
-import Control.Arrow( first )
+import Control.Arrow (first)
 
 -- TODO: probably remove "Show a" later
 class Show a => Message a where
@@ -48,7 +48,7 @@ class Show a => Message a where
     -- TODO: there must be a better way of handling this ...
     lambdabotName :: a -> Nick
 
--- |The type of nicknames isolated from a message.
+-- | The type of nicknames isolated from a message.
 data Nick
   = Nick {
         nTag :: !String, -- ^The tag of the server this nick is on
@@ -67,7 +67,7 @@ instance Eq Nick where
 instance Ord Nick where
   (Nick tag name) <= (Nick tag2 name2) =
      (tag, canonicalizeName name) <= (tag2, canonicalizeName name2)
-  
+
 
 -- Helper functions
 upckStr :: String -> String -> Nick
@@ -78,18 +78,18 @@ upckStr def str | null ac   = Nick def str
 pckStr :: Nick -> String
 pckStr nck = nTag nck ++ ':' : nName nck
 
--- |Format a nickname for display.  This will automatically omit the server
+-- | Format a nickname for display.  This will automatically omit the server
 -- field if it is the same as the server of the provided message.
 showNick :: Message a => a -> Nick -> String
 showNick msg nick_ | nTag nick_ == server msg = nName nick_
                    | otherwise                = pckStr nick_
 
--- |Parse a nickname received in a message.  If the server field is not
+-- | Parse a nickname received in a message.  If the server field is not
 -- provided, it defaults to the same as that of the message.
 readNick :: Message a => a -> String -> Nick
 readNick msg str = upckStr (server msg) str'
-	where str' | last str `elem` ":" = init str
-	           | otherwise           = str
+        where str' | last str `elem` ":" = init str
+                   | otherwise           = str
 
 instance Show Nick where
     show x | nTag x == "freenode" = show $ nName x
@@ -98,12 +98,12 @@ instance Show Nick where
 instance Read Nick where
     readsPrec prec str = map (first (upckStr "freenode")) (readsPrec prec str)
 
--- |Pack a nickname into a ByteString.  Note that the resulting strings are
+-- | Pack a nickname into a ByteString.  Note that the resulting strings are
 -- not optimally formatted for human consumtion.
 packNick :: Nick -> P.ByteString
 packNick = P.pack . pckStr
 
--- |Unpack a nickname packed by 'packNick'.
+-- | Unpack a nickname packed by 'packNick'.
 unpackNick :: P.ByteString -> Nick
 unpackNick = upckStr "freenode" . P.unpack
 
