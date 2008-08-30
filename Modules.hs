1533ca1a3056e6af8ea063a6167f26c0ffba7570
Spencer Janssen <spencerjanssen@gmail.com>
2008-08-30 00:34:05 +0000

    Add BotPP pragma to Modules.hs

--- Modules.hs
+++ Modules.hs
@@ -1,1 +1,3 @@
+{-# OPTIONS_GHC -pgmF BotPP -F #-}
+
 MODULES Base System Dict Dummy Karma Quote Seen State Topic Type Eval Babel Version More Pl Help Dice Search Vixen Fact Todo Spell Haddock Hoogle Where Elite Localtime Poll Djinn Pretty Compose Unlambda Slap Instances Fresh Tell Url Free Undo BF FT Check Pointful Source OfflineRC IRC Activity UnMtl OEIS Ticker
