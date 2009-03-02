be3a4716b224484638246bb60a1b6a6fad46986d
James Cook <mokus@deepbondi.net>
2009-03-02 14:09:12 +0000

    more PatternSignatures

--- Plugin/Todo.hs
+++ Plugin/Todo.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, PatternGuards, ScopedTypeVariables, TypeSynonymInstances #-}
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, PatternGuards, ScopedTypeVariables, PatternSignatures, TypeSynonymInstances #-}
 -- | A todo list
 --
 -- (c) 2005 Samuel Bronson
