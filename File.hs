973c5b05d388e073e71a6dc955f3f11d70c62f60
Andrea Vezzosi <sanzhiyan@gmail.com>
2009-09-09 14:23:46 +0000

    rewrite of File using System.FilePath and explicit exceptions in case of errors

--- File.hs
+++ File.hs
@@ -2,21 +2,26 @@ module File where
 
 import Control.Monad
 import System.Directory
+import System.FilePath
 import Paths_lambdabot (getDataFileName)
 
 -- | Constants.
-lambdabot, local, state :: String
-lambdabot = "/.lambdabot/"
-state = lambdabot ++ local
-local = "State/"
+lambdabot, local, state :: FilePath
+lambdabot = ".lambdabot"
+state = lambdabot </> local
+local = "State"
+
+maybeFileExists :: FilePath -> IO (Maybe FilePath)
+maybeFileExists path = 
+    do b <- doesFileExist path
+       return $ if b then Just path else Nothing
 
 -- | For a given file, look locally under State/. That is, suppose one is
 -- running out of a Lambdabot darcs repository in /home/cale/lambdabot. Then
 --
 -- > lookLocally "fact" ~> "/home/cale/lambdabot/State/fact"
 lookLocally :: FilePath -> IO (Maybe String)
-lookLocally f = do b <- doesFileExist (local ++ f)
-                   if b then return $ Just (local ++ f) else return Nothing
+lookLocally file = maybeFileExists $ local </> file
 
 -- | For a given file, look at the home directory. By default, we stash files in
 -- ~/.lambdabot. So, running Lambdabot normally would let us do:
@@ -26,29 +31,24 @@ lookLocally f = do b <- doesFileExist (local ++ f)
 -- (Note that for convenience we preserve the "State/foo" address pattern.)
 lookHome :: FilePath -> IO (Maybe String)
 lookHome f = do home <- getHomeDirectory
-                let full = home ++ state ++ f
-                b <- doesFileExist (full)
-                if b then return $ Just full else return Nothing
+                maybeFileExists $ home </> state </> f
 
 -- | Do ~/.lambdabot & ~/.lambdabot/State exist?
 isHome :: IO Bool
 isHome = do home <- getHomeDirectory
-            top <- doesDirectoryExist (home ++ lambdabot)
-            stat <- doesDirectoryExist (home ++ state)
-            return (top && stat)
+            fmap and . mapM (doesDirectoryExist . (home </>)) $ [lambdabot, state]
 
 -- | Create ~/.lambdabot and ~/.lambdabot/State
 mkdirL :: IO ()
 mkdirL = do home <- getHomeDirectory
-            createDirectory (home ++ lambdabot)
-            createDirectory (home ++ state)
+            mapM_ (createDirectory . (home </>)) $ [lambdabot, state]
 
 -- | Ask Cabal for the read-only copy of a file, and copy it into ~/.lambdabot/State.
 cpDataToHome :: FilePath -> IO ()
-cpDataToHome f = do rofile <- getDataFileName ("State/" ++ f)
+cpDataToHome f = do rofile <- getDataFileName (local </> f)
                     home <- getHomeDirectory
                     -- cp /.../lambdabot-4.foo/State/foo ~/.lambdabot/State/foo
-                    copyFile rofile (home ++ state ++ f)
+                    copyFile rofile (home </> state </> f)
 
 -- | Complicated. If a file exists locally, we return that. If a file exists in
 -- ~/lambdabot/State, we return that. If neither the file nor ~/lambdabot/State
@@ -57,7 +57,6 @@ cpDataToHome f = do rofile <- getDataFileName ("State/" ++ f)
 -- into the conventional functions easily; unfortunately, this removes
 -- error-checking, as an error is now just \"\".
 findFile :: FilePath -> IO String
-
 findFile f = do first <- lookLocally f
                 case first of
                   -- With any luck we can exit quickly
@@ -78,4 +77,7 @@ findFile f = do first <- lookLocally f
                                                 g <- lookHome f
                                                 case g of
                                                   Just a -> return a
-                                                  Nothing -> return ""
+                                                  Nothing -> 
+                                                      do home <- getHomeDirectory 
+                                                         fail $ "File.findFile: couldn't find file " 
+                                                                ++ f ++ " in " ++ home </> state
