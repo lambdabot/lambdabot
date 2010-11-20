5be00e6f69ca957c450584573d3c2026a74736c6
Derek Elkins <derek.a.elkins@gmail.com>
2010-11-20 09:18:41 +0000

    Search.hs rewrite.  Adds a dependency on HTTP and TagSoup.  The latter was already indirectly depended on via lambdabot-utils.

--- Plugin/Search.hs
+++ Plugin/Search.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, ViewPatterns #-}
 -- | Search various things, Wikipedia and google for now.
 --
 -- (c) 2005 Samuel Bronson
@@ -9,77 +9,92 @@
 module Plugin.Search (theModule) where
 
 import Plugin
-import Control.Monad            (mplus)
-import qualified Text.Regex as R
+import Network.HTTP
+import Text.HTML.TagSoup
+import Text.HTML.TagSoup.Match (anyAttr, tagOpen)
+import Data.Maybe (listToMaybe)
 
 $(plugin "Search")
 
-engines :: [(String, (String, String))]
+engines :: [(String, (URI, String -> String, [Header]))]
 engines =
-   [("google"
-   ,("http://www.google.com/search?hl=en&q=","&btnI=I%27m+Feeling+Lucky")),
-
-    ("wikipedia"
-   ,("http://en.wikipedia.org/wiki/Special:Search?search=", "")),
-
-    ("gsite"
-   ,("http://www.google.com/search?hl=en&q=site%3A", "&btnI=I%27m+Feeling+Lucky"))
+   [("google", (googleUri, (\s -> "?hl=en&q="++s++"&btnI=I'm+Feeling+Lucky"), googleHeaders)),
+    -- ("wikipedia", (wikipediaUri, ("?search="++), [])), -- this has changed and Wikipedia requires a User-Agent string
+    ("gsite", (googleUri, (\s -> "?hl=en&q=site%3A"++s++"&btnI=I'm+Feeling+Lucky"), googleHeaders)),
+    ("gwiki", (googleUri, (\s -> "?hl=en&q=site%3Ahaskell.org/haskellwiki+" ++s++"&btnI=I'm+Feeling+Lucky"), googleHeaders))
    ]
+
+googleHeaders :: [Header]
+googleHeaders = [mkHeader HdrReferer "http://www.google.com/"]
+
+normalizeOptions :: NormalizeRequestOptions a
+normalizeOptions = defaultNormalizeRequestOptions {
+    normDoClose = True,
+    normForProxy = isJust (proxy config),
+    normUserAgent = Nothing } -- there is a default user agent, perhaps we want it?
+
+makeUri :: String -> String -> URI
+makeUri regName path = nullURI {
+    uriScheme = "http:",
+    uriAuthority = Just (URIAuth { uriUserInfo = "", uriRegName = regName, uriPort = "" }),
+    uriPath = path }
+
+googleUri :: URI
+googleUri = makeUri "www.google.com" "/search"
+-- wikipediaUri = makeUri "en.wikipedia.org" "/wiki/Special:Search"
 
 instance Module SearchModule () where
     moduleHelp _ s      = case s of
          "google"    -> "google <expr>. Search google and show url of first hit"
-         "wikipedia" -> "wikipedia <expr>. Search wikipedia and show url of first hit"
+         -- "wikipedia" -> "wikipedia <expr>. Search wikipedia and show url of first hit"
          "gsite"     -> "gsite <site> <expr>. Search <site> for <expr> using google"
-         "gwiki"     -> "wiki <expr>. Search (new) haskell.org wiki for <expr> using google."
-    moduleCmds      _   = "gwiki" : map fst engines
-    process_ _ "gwiki" e = lift $ ((. dropSpace) . searchCmd) "gsite" ("haskell.org/haskellwiki" ++ e)
-    process_ _ s       e = lift $ ((. dropSpace) . searchCmd) s e
+         "gwiki"     -> "gwiki <expr>. Search (new) haskell.org wiki for <expr> using google."
+    moduleCmds _ = map fst engines
+    process_ _ s e = lift $ searchCmd s (dropSpace e)
 
 ------------------------------------------------------------------------
 
 searchCmd :: String -> String -> LB [String]
-searchCmd _ []        = return ["Empty search."]
-searchCmd engine rest = do
-    headers <- io $ queryit "HEAD" engine rest
-    body    <- io $ queryit "GET" engine rest
-    case getHeader "Location" headers `mplus` extractConversion body of
-      Just url -> do
-        title <- io $ runWebReq (urlPageTitle url) (proxy config)
-        return $ maybe [url] (\t -> [url, t]) title
-      Nothing  -> return ["No Result Found."]
+searchCmd _          []   = return ["Empty search."]
+searchCmd engineName (Network.HTTP.urlEncode -> query)
+    | engineName == "google" = -- for Google we do both to get conversions, e.g. for '3 lbs in kg'
+        doHTTP request $ \response -> 
+        case response of
+            Response { rspCode = (3,0,2), rspHeaders = (lookupHeader HdrLocation -> Just url) } ->
+                doGoogle >>=  handleUrl url
+            _ -> fmap (\extra -> if null extra then ["No Result Found."] else extra) doGoogle
+    | otherwise = 
+        doHTTP request $ \response -> 
+        case response of
+            Response { rspCode = (3,0,2), rspHeaders = (lookupHeader HdrLocation -> Just url) } ->
+                handleUrl url []
+            _ -> return ["No Result Found."]
+  where handleUrl url extra = do
+            title <- io $ runWebReq (urlPageTitle url) (proxy config)
+            return $ extra ++ maybe [url] (\t -> [url, t]) title
+        Just (uri, makeQuery, headers) = lookup engineName engines
+        request = normalizeRequest normalizeOptions $ Request {
+            rqURI = uri { uriQuery = makeQuery query },
+            rqMethod = HEAD,
+            rqHeaders = headers,
+            rqBody = "" }
+        doGoogle = 
+            doHTTP (request { rqMethod = GET, rqURI = uri { uriQuery = "?hl=en&q=" ++ query } }) $ \response ->
+                case response of
+                    Response { rspCode = (2,_,_), rspBody = (extractConversion -> Just result) } ->
+                        return [result]
+                    _ -> return []
 
-queryUrl :: String -> String -> String
-queryUrl engine q = prefix ++ urlEncode q ++ suffix
-    where
-    (prefix, suffix) = fromMaybe (error "search: invalid command")
-                                 (lookup engine engines)
+doHTTP :: HStream a => Request a -> (Response a -> LB [String]) -> LB [String]
+doHTTP request handler = do
+    result <- io $ simpleHTTP request
+    case result of
+        Left connError -> return ["Connection error: "++show connError]
+        Right response -> handler response
 
-queryit :: String -> String -> String -> IO [String]
-queryit meth engine q = readPage (proxy config) uri request ""
-    where url = queryUrl engine q
-          Just uri = parseURI url
-          abs_path = uriPath uri ++ uriQuery uri ++ uriFragment uri
-          request  = case proxy config of
-                        Nothing -> [meth ++ " " ++ abs_path ++ " HTTP/1.0", ""]
-                        _       -> [meth ++ " " ++ url ++ " HTTP/1.0", ""]
-
-extractConversion :: [String] -> Maybe String
-extractConversion [] = error "conv: No response, something weird is up."
-extractConversion ls = (getConv $ last ls) >>= return . pipeline replaceFuncs
-    where
-        regex1 = regex' "<font size=\\+1><b>"
-        regex2 = regex' "</b>"
-
-        getConv a = do
-            (_,_,s,_)  <- R.matchRegexAll regex1 a
-            (s',_,_,_) <- R.matchRegexAll regex2 s
-            return s'
-
-        searchAndReplace new re = \s -> R.subRegex (regex' re) s new
-        replaceFuncs = zipWith searchAndReplace
-                            [    "^",       "",      "x",                      ","]
-                            ["<sup>", "</sup>", "&#215;", "<font size=-2> </font>"]
-
-        pipeline [] a = a
-        pipeline (f:fs) a = pipeline fs $ f a
+-- This is clearly fragile.
+extractConversion :: String -> Maybe String
+extractConversion (parseTags -> tags) = listToMaybe [txt |
+    section <- sections (tagOpen ("h2"==) (anyAttr (\(name, value) -> name == "class" && value == "r"))) tags,
+    txt <- [dropSpace $ drop 1 $ dropWhile (/= '=') t | TagText t <- section],
+    not (null txt)]
