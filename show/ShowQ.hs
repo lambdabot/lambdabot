68d9f553705b6ae242f8f98a3c8d5fefba5e705e
gwern0 <gwern0@gmail.com>
2011-01-19 02:21:38 +0000

    ShowQ: warning obsolete because of mueval

--- show/ShowQ.hs
+++ show/ShowQ.hs
@@ -1,12 +1,8 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 -- Helper code for runplugs
---
--- Note: must be kept in separate module to hide unsafePerformIO from
--- runplugs-generated eval code!! you're warned.
 
 module ShowQ where
 
--- import Language.Haskell.TH
 import System.IO.Unsafe
 import Data.Ratio
 
