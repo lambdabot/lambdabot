c81b1096c3694da2c122987fc5a4229c507de4eb
James Cook <mokus@deepbondi.net>
2012-02-25 16:56:07 +0000

    reintroduced sanity check in @check and cleaned up output to account for quirks of 'show' package's "myquickcheck"

--- Plugin/Check.hs
+++ Plugin/Check.hs
@@ -1,17 +1,13 @@
-{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, PatternGuards #-}
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses #-}
 -- Copyright (c) 6 DonStewart - http://www.cse.unsw.edu.au/~dons
 -- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
 
 -- | Test a property with QuickCheck
 module Plugin.Check where
 
-import File (findFile)
 import Plugin
 import Plugin.Eval (plugs)
 import Lambdabot.Parser
-
-import qualified Text.Regex as R
-import Codec.Binary.UTF8.String (decodeString)
 
 $(plugin "Check")
 
@@ -20,8 +16,8 @@ instance Module CheckModule () where
     moduleHelp _ _     = "check <expr>\nYou have QuickCheck and 3 seconds. Prove something."
     process _ _ to _ s = ios80 to (check s)
 
-binary :: String
-binary = "mueval"
-
 check :: String -> IO String
-check src = plugs ("myquickcheck $ " ++ src)
+check src = 
+    case parseExpr src of
+        Left e  -> return e
+        Right _ -> plugs ("myquickcheck (" ++ src ++ ") `seq` hsep[]")
