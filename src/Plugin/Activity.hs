aba6e7e91be2672c2d9765b251c67eb7b7fb5cf8
James Cook <mokus@deepbondi.net>
2012-03-02 00:28:12 +0000

    Minor style changes in Activity plugin

--- src/Plugin/Activity.hs
+++ src/Plugin/Activity.hs
@@ -6,7 +6,6 @@ import Plugin
 import qualified Lambdabot.Message as Msg
 
 import Control.Arrow ((&&&))
-import Data.Maybe (fromMaybe)
 import Control.Exception (evaluate)
 
 import System.Time
@@ -19,6 +18,8 @@ helpStr = "activity seconds. Find out where/how much the bot is being used"
 
 instance Module ActivityModule where
     type ModuleState ActivityModule = ActivityState
+    moduleDefState _ = return []
+    moduleInit _ = bindModule2 activityFilter >>= ircInstallOutputFilter
     
     moduleCmds _ = 
         [ (command "activity") 
@@ -31,24 +32,25 @@ instance Module ActivityModule where
             , process = activity True
             }
         ]
-    moduleDefState _            = return []
-    moduleInit   _              = bindModule2 activityFilter >>=
-                                      ircInstallOutputFilter
 
 activity full args = do
+    let obscure nm
+            | full || isPrefixOf "#" (Msg.nName nm) = return nm
+            | otherwise = readNick "private"
+    
     TOD secs ps <- io getClockTime
     let cutoff = TOD (secs - (fromMaybe 90 $ readM args)) ps
     users <- mapM (obscure . snd) . takeWhile ((> cutoff) . fst) =<< lift readMS
     let agg_users = reverse . sort . map (length &&& head) . group . sort $ users
     fmt_agg <- fmap (concatWith " " . (:) (show (length users) ++ "*total"))
                     (mapM (\(n,u) -> do u' <- showNick u; return (show n ++ "*" ++ u')) $ agg_users)
+    
     say fmt_agg
-  where obscure nm | full || isPrefixOf "#" (Msg.nName nm) = return nm
-                   | otherwise = readNick "private"
 
 activityFilter :: Msg.Nick -> [String] -> ModuleLB ActivityModule
-activityFilter target lns = do io $ evaluate $ foldr seq () $ map (foldr seq ()) $ lns
-                               withMS $ \ st wr -> do
-                                 now <- io getClockTime
-                                 wr (map (const (now,target)) lns ++ st)
-                               return lns
+activityFilter target lns = do
+    io $ evaluate $ foldr seq () $ map (foldr seq ()) $ lns
+    withMS $ \ st wr -> do
+        now <- io getClockTime
+        wr (map (const (now,target)) lns ++ st)
+    return lns
