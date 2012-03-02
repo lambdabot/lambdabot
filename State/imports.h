912b33a73cde53936ab51c0b1a8030f31c066922
James Cook <mokus@deepbondi.net>
2012-03-02 10:28:14 +0000

    updated imports.h to match Pristine.h

--- State/imports.h
+++ State/imports.h
@@ -1,8 +1,13 @@
-import qualified Prelude as P
-import Prelude hiding (mapM, sequence, mapM_, sequence_, (.), (++), map)
-import Numeric
-import System.Random
-import Data.Array
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+import Control.Monad.Cont
+import Control.Monad.Identity
+import Control.Monad.Instances
+import Control.Monad.Reader
+import Control.Monad.ST.Safe
+import Control.Monad.State
+import Control.Monad.Writer
 import Data.Bits
 import Data.Bool
 import Data.Char
@@ -10,70 +15,31 @@ import Data.Complex
 import Data.Dynamic
 import Data.Either
 import Data.Eq
-import Data.Fixed
-import qualified Data.Foldable
-import Data.Function hiding ((.))
-import Data.Ord
-import qualified Data.Generics
-import Data.Generics hiding (GT)
-import Data.Graph
+import Data.Function
 import Data.Int
-import Data.Ix
-import Data.List hiding ((++),map)
+import Data.List
 import Data.Maybe
 import Data.Monoid
+import Data.Ord
 import Data.Ratio
-import qualified Data.Sequence
+import Data.STRef
 import Data.Tree
 import Data.Tuple
-import qualified Data.Traversable
 import Data.Typeable
 import Data.Word
-import Data.STRef
-import qualified Data.Map as M
-import qualified Data.Set as S
-import qualified Data.IntSet as IS
-import qualified Data.IntMap as IM
+import Numeric
+import System.Random
+import Trusted {- from "lambdabot-trusted" package in lambdabot source tree -}
+import Text.PrettyPrint.HughesPJ hiding (empty)
+import Text.Printf
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Lazy.Char8 as BSLC
-import Data.Number.Symbolic
-import Data.Number.Dif
-import Data.Number.CReal
-import Data.Number.Fixed
-import Data.Number.Interval
-import Data.Number.BigFloat
-import Control.Monad
-import Control.Monad.Cont
-import Control.Monad.Identity
-import Control.Monad.State
-import Control.Monad.State
-import Control.Monad.ST (ST, runST, fixST)
-import Control.Monad.Reader
-import Control.Monad.Fix
-import Control.Monad.Writer
-import Control.Monad.RWS
-import Control.Monad.Error
-import Control.Monad.Instances
-import Control.Monad.Logic
-import Control.Arrow hiding (pure)
-import qualified Control.Arrow.Transformer as AT
-import qualified Control.Arrow.Transformer.All as AT
-import Control.Arrow.Operations
-import Control.Applicative
-import Control.Parallel
-import Control.Parallel.Strategies
-import Text.Printf
-import Text.PrettyPrint.HughesPJ hiding (empty)
-import Test.QuickCheck
-import ShowQ
-
-import SimpleReflect hiding (var)
-import Math.OEIS
-
-import Data.VectorSpace
-import Data.NumInstances
-import Data.LinearMap
-
-import Data.Fixed
+import qualified Data.Foldable
+import qualified Data.IntMap as IM
+import qualified Data.IntSet as IS
+import qualified Data.Map as M
+import qualified Data.Sequence
+import qualified Data.Set as S
+import qualified Data.Traversable
