79eacfeca3eb1d68b13ca7524ef506380d454b73
James Cook <mokus@deepbondi.net>
2013-02-21 22:07:03 -0800

    slight improvement to error message for empty @do...
    ... and other simple code-mangling commands

--- src/Lambdabot/Parser.hs
+++ src/Lambdabot/Parser.hs
@@ -10,10 +10,11 @@ import Language.Haskell.Exts
 -- |Parse a string as an 'Exp' or a 'Decl', apply the given generic transformation to it,
 -- and re-render it back to text.
 withParsed :: (forall a. (Data a, Eq a) => a -> a) -> String -> String
-withParsed f s = case (parseExp s, parseDecl s) of
-                    (ParseOk a, _) -> prettyPrintInLine $ f a
-                    (_, ParseOk a) -> prettyPrintInLine $ f a
-                    (ParseFailed l e,  _) -> prettyPrint l ++ ':' : e
+withParsed _ "" = "Error: expected a Haskell expression or declaration"
+withParsed f s  = case (parseExp s, parseDecl s) of
+    (ParseOk a, _)          -> prettyPrintInLine $ f a
+    (_, ParseOk a)          -> prettyPrintInLine $ f a
+    (ParseFailed l e,  _)   -> prettyPrint l ++ ':' : e
 
 -- |Render haskell code in a compact format
 prettyPrintInLine :: Pretty a => a -> String
