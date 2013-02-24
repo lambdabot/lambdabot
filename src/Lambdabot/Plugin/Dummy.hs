bb011be39625797c2b919f30a09b273c5efd0446
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-02-24 11:39:59 +0100

    Clean up Dummy plugin

--- src/Lambdabot/Plugin/Dummy.hs
+++ src/Lambdabot/Plugin/Dummy.hs
@@ -13,7 +13,7 @@ theModule = newModule
     { moduleCmds = return
         $ (command "eval")
             { help = say "eval. Do nothing (perversely)"
-            , process = const (return ()) 
+            , process = const (return ())
             }
         : (command "choose")
             { help = say "choose. Lambdabot featuring AI power"
@@ -27,69 +27,54 @@ theModule = newModule
             }
           | (cmd, op) <- dummylst
           ]
-    
+
     , contextual = \msg -> case msg of
-        "lisppaste2: url"  -> say pastebinMsg
+        "lisppaste2: url"  -> say "Haskell pastebin: http://hpaste.org/"
         _                  -> return ()
     }
-
-pastebinMsg :: String
-pastebinMsg = "Haskell pastebin: http://hpaste.org/"
 
 dummyHelp s = case s of
     "dummy"       -> "dummy. Print a string constant"
     "bug"         -> "bug. Submit a bug to GHC's trac"
-
     "id"          -> "id <arg>. The identity plugin"
     "show"        -> "show <foo>. Print \"<foo>\""
     "wiki"        -> "wiki <page>. URLs of Haskell wiki pages"
-    "oldwiki"     -> "oldwiki <page>. URLs of the old hawiki pages"
     "paste"       -> "paste. Paste page url"
-
     "docs"        -> "docs <lib>. Lookup the url for this library's documentation"
-    "source"      -> "source <lib>. Lookup the url of fptools libraries"
-    "fptools"     -> "fptools <lib>. Lookup url of ghc base library modules"
-
     "learn"       -> "learn. The learning page url"
-    "eurohaskell" -> "eurohaskell. Historical"
-    "map"         -> "map. #haskell user map"
+    "haskellers"  -> "haskellers. Find other Haskell users"
     "botsnack"    -> "botsnack. Feeds the bot a snack"
     "get-shapr"   -> "get-shapr. Summon shapr instantly"
     "shootout"    -> "shootout. The debian language shootout"
     "faq"         -> "faq. Answer frequently asked questions about Haskell"
     "googleit"    -> "letmegooglethatforyou."
+    "hackage"     -> "find stuff on hackage"
+    _             -> "I'm sorry Dave, I'm affraid I don't know that command"
 
 dummylst :: [(String, String -> String)]
 dummylst =
-    [("id",      (' ' :) . id)
-    ,("show",       show)
-
-    ,("dummy",      const "dummy")
-    ,("bug",        const "http://hackage.haskell.org/trac/ghc/newticket?type=bug")
-    ,("get-shapr",  const "shapr!!")
-    ,("faq",        const "The answer is: Yes! Haskell can do that.")
-    ,("paste",      const pastebinMsg)
-    ,("learn",      const "http://www.haskell.org/learning.html")
-    ,("map",        const "http://www.haskell.org/hawiki/HaskellUserLocations")
-    ,("shootout",   const "http://shootout.alioth.debian.org/gp4/benchmark.php?test=all&lang=all")
-    ,("botsnack",   const ":)")
-    ,("thanks",     const "you are welcome")
-    ,("thx",        const "you are welcome")
-    ,("thank you",  const "you are welcome")
-    ,("ping",	    const "pong")
-
-    ,("wiki",       lookupWiki)
-    ,("oldwiki",     ("http://www.haskell.org/hawiki/" ++))
-
-    ,("docs",        \x -> if null x
-                            then docPrefix </> "index.html"
-                            else lookupPackage docPrefix '-' "html" x)
-
-    ,("source",     lookupPackage "http://darcs.haskell.org/packages" '/' "hs")
-
-    ,("fptools",    lookupPackage "http://darcs.haskell.org/packages" '/' "hs")
-    ,("hackage",    lookupHackage)
-    ,("googleit",   lookupGoogle)
+    [("dummy"      , const "dummy")
+    ,("bug"        , const "http://hackage.haskell.org/trac/ghc/newticket?type=bug")
+    ,("id"         , (' ' :) . id)
+    ,("show"       , show)
+    ,("wiki"       , lookupWiki)
+    ,("paste"      , const "Haskell pastebin: http://hpaste.org/")
+    ,("docs"       , \x -> if null x
+                           then docPrefix </> "index.html"
+                           else lookupPackage docPrefix '-' "html" x)
+    ,("learn"      , const "http://www.haskell.org/haskellwiki/Learning_Haskell")
+    ,("haskellers" , const "http://www.haskellers.com/")
+    ,("botsnack"   , const ":)")
+    ,("get-shapr"  , const "shapr!!")
+    ,("shootout"   , const "http://shootout.alioth.debian.org/gp4/benchmark.php?test=all&lang=all")
+    ,("faq"        , const "The answer is: Yes! Haskell can do that.")
+    ,("googleit"   , lookupGoogle)
+    ,("hackage"    , lookupHackage)
+    ,("thanks"     , const "you are welcome")
+    ,("thx"        , const "you are welcome")
+    ,("thank you"  , const "you are welcome")
+    ,("ping"       , const "pong")
+    ,("tic-tac-toe", const "how about a nice game of chess?")
     ]
 
 lookupWiki :: String -> String
@@ -113,11 +98,12 @@ docPrefix :: String
 docPrefix = "http://haskell.org/ghc/docs/latest/html/libraries"
 
 lookupPackage :: String -> Char -> String -> String -> String
-lookupPackage begin sep end x'
- = case M.lookup (P.pack x) docAssocs of
+lookupPackage begin sep end x''
+ = case M.lookup (P.pack x') docAssocs of
         Nothing -> x ++ " not available"
         Just m  -> begin
                     </> P.unpack m
-                    </> map (choice (=='.') (const sep) id) x
+                    </> map (choice (=='.') (const sep) id) x'
                     <.> end
- where x = dropSpace x'
+ where x  = dropSpace x''
+       x' = map toLower x
