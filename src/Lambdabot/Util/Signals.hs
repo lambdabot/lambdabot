693408181e0d8e786d484a3c84bb7874d570d159
James Cook <mokus@deepbondi.net>
2013-03-23 19:24:01 -0700

    some style changes in Signals module

--- src/Lambdabot/Util/Signals.hs
+++ src/Lambdabot/Util/Signals.hs
@@ -49,10 +49,10 @@ instance Exception SignalException
 -- A bit of sugar for installing a new handler
 --
 withHandler :: MonadBaseControl IO m => Signal -> Handler -> m () -> m ()
-withHandler s h m
-  = bracket (liftBase (installHandler s h Nothing))
-            (liftBase . flip (installHandler s) Nothing)
-            (const m)
+withHandler s h m = bracket
+    (liftBase (installHandler s h Nothing))
+    (liftBase . flip (installHandler s) Nothing)
+    (const m)
 
 -- And more sugar for installing a list of handlers
 withHandlerList :: MonadBaseControl IO m => [Signal] -> (Signal -> Handler) -> m () -> m ()
@@ -64,32 +64,24 @@ withHandlerList sl h m = foldr (withHandler `ap` h) m sl
 -- Be careful adding signals, some signals can't be caught and
 -- installHandler just raises an exception if you try
 --
-ircSignalsToCatch :: [Signal]
-ircSignalsToCatch = [
-    busError,
-    segmentationViolation,
-    keyboardSignal,
-    softwareTermination,
-    keyboardTermination,
-    lostConnection,
-    internalAbort
+ircSignalsToCatch :: [(Signal, String)]
+ircSignalsToCatch = 
+    [ (busError,              "SIGBUS"  )
+    , (segmentationViolation, "SIGSEGV" )
+    , (keyboardSignal,        "SIGINT"  )
+    , (softwareTermination,   "SIGTERM" )
+    , (keyboardTermination,   "SIGQUIT" )
+    , (lostConnection,        "SIGHUP"  )
+    , (internalAbort,         "SIGABRT" )
     ]
 
 --
 -- User friendly names for the signals that we can catch
 --
-ircSignalMessage :: Signal -> [Char]
-ircSignalMessage s
-   | s==busError              = "SIGBUS"
-   | s==segmentationViolation = "SIGSEGV"
-   | s==keyboardSignal        = "SIGINT"
-   | s==softwareTermination   = "SIGTERM"
-   | s==keyboardTermination   = "SIGQUIT"
-   | s==lostConnection        = "SIGHUP"
-   | s==internalAbort         = "SIGABRT"
--- this case shouldn't happen if the list of messages is kept up to date
--- with the list of signals caught
-   | otherwise                  = "killed by unknown signal"
+ircSignalMessage :: Signal -> String
+ircSignalMessage sig = case lookup sig ircSignalsToCatch of
+    Just sigName -> sigName
+    Nothing      -> "killed by unknown signal"
 
 --
 -- The actual signal handler. It is this function we register for each
@@ -100,7 +92,7 @@ ircSignalMessage s
 --
 ircSignalHandler :: ThreadId -> Signal -> Handler
 ircSignalHandler threadid s
-  = CatchOnce $ do
+    = CatchOnce $ do
         putMVar catchLock ()
         releaseSignals
         throwTo threadid $ SignalException s
@@ -109,9 +101,10 @@ ircSignalHandler threadid s
 -- | Release all signal handlers
 --
 releaseSignals :: IO ()
-releaseSignals =
-    flip mapM_ ircSignalsToCatch
-               (\sig -> installHandler sig Default Nothing)
+releaseSignals = sequence_ 
+    [ installHandler sig Default Nothing
+    | (sig, _) <- ircSignalsToCatch
+    ]
 
 --
 -- Mututally exclusive signal handlers
@@ -132,5 +125,5 @@ withIrcSignalCatch m = do
     _ <- liftBase $ installHandler sigPIPE Ignore Nothing
     _ <- liftBase $ installHandler sigALRM Ignore Nothing
     threadid <- myThreadId
-    withHandlerList ircSignalsToCatch (ircSignalHandler threadid) m
+    withHandlerList (map fst ircSignalsToCatch) (ircSignalHandler threadid) m
 #endif
