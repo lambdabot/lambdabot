b0a1250363ba38c0802880d3e403c599126acc30
Bertram Felgenhauer <int-e@gmx.de>
2014-04-13 00:40:21 +0200

    add current configuration and supporting scripts

---
+++ lambdabot/scripts/online/run-offline.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+LC_ALL=en_US.UTF-8 PATH=$HOME/.cabal/bin:$HOME/bin:$PATH \
+  exec lambdabot \
+  --trust=base \
+  --trust=array \
+  --trust=bytestring \
+  --trust=containers \
+  --trust=text \
+  --trust=random \
+  --trust=parallel \
+  --trust=stm \
+  --trust=unordered-containers \
+  --trust=hashable \
+  --trust=exceptions \
+  --trust=prelude-extras \
+  --trust=base-orphans \
+  --trust=bifunctors \
+  --trust=profunctors \
+  --trust=reflection \
+  --trust=adjunctions \
+  --trust=kan-extensions \
+  --trust=lens \
+  --trust=lambdabot-trusted \
+  -X BangPatterns \
+  -X ConstrainedClassMethods \
+  -X ConstraintKinds \
+  -X DataKinds \
+  -X DeriveDataTypeable \
+  -X DeriveFoldable \
+  -X DeriveFunctor \
+  -X DeriveGeneric \
+  -X DeriveTraversable \
+  -X EmptyCase \
+  -X EmptyDataDecls \
+  -X ExistentialQuantification \
+  -X ExtendedDefaultRules \
+  -X FlexibleContexts \
+  -X FlexibleInstances \
+  -X FunctionalDependencies \
+  -X GADTs \
+  -X ImplicitParams \
+  -X ImplicitPrelude \
+  -X KindSignatures \
+  -X LambdaCase \
+  -X LiberalTypeSynonyms \
+  -X MagicHash \
+  -X MultiParamTypeClasses \
+  -X NoMonomorphismRestriction \
+  -X PackageImports \
+  -X ParallelListComp \
+  -X PatternGuards \
+  -X PolyKinds \
+  -X PolymorphicComponents \
+  -X PostfixOperators \
+  -X RankNTypes \
+  -X ScopedTypeVariables \
+  -X StandaloneDeriving \
+  -X TupleSections \
+  -X TypeFamilies \
+  -X TypeOperators \
+  -X TypeSynonymInstances \
+  -X UnboxedTuples \
+  -X UndecidableInstances \
+  -X UnicodeSyntax \
+  -X ViewPatterns
+# --trust=semigroups
+# --trust=tagged
+# --trust=comonad
