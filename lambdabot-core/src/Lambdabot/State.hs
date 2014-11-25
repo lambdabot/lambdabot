4c694890466499e9d4624e9b3d89a50fba82245e
Bertram Felgenhauer <int-e@gmx.de>
2014-11-25 05:35:28 +0100

    make writeGlobalState more robust
    State is now written to a temporary file and moved into place, making
    the operation of writing the state atomic.

--- lambdabot-core/src/Lambdabot/State.hs
+++ lambdabot-core/src/Lambdabot/State.hs
@@ -42,6 +42,9 @@ import Control.Monad.Reader
 import Control.Monad.Trans.Control
 import qualified Data.ByteString.Char8 as P
 import Data.IORef.Lifted
+import qualified System.IO as I
+import qualified System.FilePath as I
+import qualified System.Directory as I
 
 -- | Thread-safe modification of an MVar.
 withMWriter :: MonadBaseControl IO m => MVar a -> (a -> (a -> m ()) -> m b) -> m b
@@ -188,7 +191,9 @@ writeGlobalState = do
                 Nothing  -> return ()   -- do not write any state
                 Just out -> do
                     stateFile <- lb (findLBFileForWriting mName)
-                    io (P.writeFile stateFile out)
+                    (stateFile', hdl) <- io (I.openBinaryTempFileWithDefaultPermissions (I.takeDirectory stateFile) (I.takeFileName stateFile))
+                    io (P.hPutStr hdl out >> I.hClose hdl)
+                    io (I.renameFile stateFile' stateFile)
 
 -- | Read it in
 readGlobalState :: Module st -> String -> LB (Maybe st)
