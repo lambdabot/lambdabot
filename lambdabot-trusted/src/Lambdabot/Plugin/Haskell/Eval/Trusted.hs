c93221f5a7b3ff958aae07f5693e89f6905320b9
Bertram Felgenhauer <int-e@gmx.de>
2015-04-03 00:11:46 +0200

    customize L.P.Haskell.Eval.Trusted
    - import random things from GHC.Exts
    - remove OEIS (since mueval is sandboxed without network access)

--- lambdabot-trusted/src/Lambdabot/Plugin/Haskell/Eval/Trusted.hs
+++ lambdabot-trusted/src/Lambdabot/Plugin/Haskell/Eval/Trusted.hs
@@ -1,17 +1,14 @@
 {-# LANGUAGE Trustworthy #-}
 module Lambdabot.Plugin.Haskell.Eval.Trusted
-    ( module Math.OEIS
-    , module Test.QuickCheck.Safe
+    ( module Test.QuickCheck.Safe
     , module Lambdabot.Plugin.Haskell.Check.ShowQ
     , module Lambdabot.Plugin.Haskell.Eval.Trusted
+    , module GHC.Exts, Constraint
     ) where
 
-import Math.OEIS
 import Lambdabot.Plugin.Haskell.Check.ShowQ
 import Test.QuickCheck.Safe
-
-describeSequence :: SequenceData -> Maybe String
-describeSequence = fmap description . lookupSequence
+import GHC.Exts (Constraint, IsList (..), IsString (..))
 
 newtype Mu f = In { out :: f (Mu f) }
 
