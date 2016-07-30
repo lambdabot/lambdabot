f60dd8446d53bf50dde1698b93749522844a07ee
Bertram Felgenhauer <int-e@gmx.de>
2016-07-30 14:06:32 +0200

    more useful @oeis output
    The output is now split into two lines.
    The first line contains a link to the oeis page and a description.
    The second line contains the actual sequence.

--- lambdabot-reference-plugins/src/Lambdabot/Plugin/Reference/OEIS.hs
+++ lambdabot-reference-plugins/src/Lambdabot/Plugin/Reference/OEIS.hs
@@ -5,6 +5,7 @@ module Lambdabot.Plugin.Reference.OEIS (oeisPlugin) where
 import Lambdabot.Plugin
 
 import Math.OEIS
+import Data.Char
 
 oeisPlugin :: Module ()
 oeisPlugin = newModule
@@ -12,7 +13,22 @@ oeisPlugin = newModule
         [ (command "oeis")
             { aliases = ["sequence"]
             , help = say "oeis <sequence>. Look up a sequence in the Online Encyclopedia of Integer Sequences"
-            , process = ios80 . fmap concat . lookupOEIS
+            , process = ios80 . lookupOEIS'
             }
         ]
     }
+
+lookupOEIS' :: String -> IO String
+lookupOEIS' a = do
+    let a' = commas . reverse . dropWhile isSpace . reverse . dropWhile isSpace $ a
+    x <- searchSequence_IO a'
+    case x of
+        Nothing -> return "Sequence not found."
+        Just s  -> return $ unlines [
+            concat ("https://oeis.org/" : take 1 (catalogNums s)) ++
+            ' ' : description s,
+            show $ sequenceData s]
+  where
+    commas []                     = []
+    commas (x:' ':xs) | isDigit x = x : ',' : commas xs
+    commas (x:xs)                 = x : commas xs
